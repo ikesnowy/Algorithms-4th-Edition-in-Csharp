eaf7f277584a758f2adf062d036835bbfbfae34c
Ike <ikesnowy@outlook.com>
2017-05-30 13:52:05 +0800

    1.1.2

--- 1.1.1/Program.cs
+++ 1.1.1/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace _1._1._1
 {
+    /*
+     * 1.1.1 给出以下表达式的值
+     * 
+     * a. (0 + 15) / 2
+     * b. 2.0e-6 * 100000000.1
+     * c. true && false || true && true
+     */
     class Program
     {
         static void Main(string[] args)
         {
+            int a = (0 + 15) / 2;
+            double b = Math.Pow(2.0, -6) * 100000000.1; //Math.Pow(double x, double y) 求x的y次方
+            bool c = true && false || true && true;
+
+            //Console.WriteLine 向控制台窗口输出一行
+            Console.WriteLine($"a.{a}");
+            Console.WriteLine($"b.{b}");
+            Console.WriteLine($"c.{c}");
         }
     }
 }

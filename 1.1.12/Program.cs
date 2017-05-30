158a500f61107a95e1af406bfc19fcafc89eb9ba
Ike <ikesnowy@outlook.com>
2017-05-30 16:37:17 +0800

    1.1.12 & 1.1.13

---
+++ 1.1.12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._12
+{
+    /*
+     * 1.1.12
+     * 
+     * 以下代码段会打印出什么结果？
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = 9 - i;
+            }
+            //a[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = a[a[i]];
+            }
+            //a[0] = a[9] = 0; a[1] = a[8] = 1; a[2] = a[7] = 2;......
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
---
+++ 1.1.13/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._13
+{
+    /*
+     * 1.1.13
+     * 
+     * 编写一段代码，打印出一个 M 行 N 列的二维数组的转置
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = 2;
+            int N = 3;
+            int[,] array = new int[M, N];
+
+            //新建一个二维数组
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = i + j;
+                }
+            }
+
+            Console.WriteLine("原二维数组：");
+            PrintArray2D(array, M, N);
+
+            Console.WriteLine("转置后：");
+            PrintArrayTranspose2D(array, M, N);
+        }
+
+        //转置输出
+        private static void PrintArrayTranspose2D(int[,] array, int rows, int columns)
+        {
+            //交换行、列输出顺序
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Console.Write($"\t{array[j, i]}");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        //正常输出
+        private static void PrintArray2D(int[,] array, int rows, int columns)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"\t{array[i, j]}");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}

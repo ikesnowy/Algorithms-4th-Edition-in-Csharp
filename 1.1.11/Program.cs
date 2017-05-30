e1a815558a9bed71ba76b5f06622b8be49313cfa
Ike <ikesnowy@outlook.com>
2017-05-30 16:15:05 +0800

    1.1.11

---
+++ 1.1.11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._11
+{
+    /*
+     * 1.1.11 
+     * 
+     * 编写一段代码，打印出一个二维布尔数组的内容。
+     * 其中，使用 * 表示真，空格表示假，打印出行号和列号
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] array = new bool[,]
+            {
+                { true, true },
+                { false, false },
+                { true, false }
+            };
+
+            PrintArray2D(array);//打印二维数组
+        }
+
+        static void PrintArray2D(bool[,] array)
+        {
+            int rows = array.GetLength(0);//获取行数
+            int columns = array.GetLength(1);//获取列数
+
+            //输出列号
+            for (int i = 0; i < columns; i++)
+            {
+                Console.Write($"\t{i + 1}");
+            }
+
+            Console.Write("\n");
+
+            for (int i = 0; i < rows; i++)
+            {
+                //输出行号
+                Console.Write($"{i + 1}");
+                for (int j = 0; j < columns; j++)
+                {
+                    if (array[i, j])
+                    {
+                        Console.Write($"\t*");
+                    }
+                    else
+                    {
+                        Console.Write($"\t ");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}

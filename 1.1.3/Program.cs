b476c3cb85cc876ddf545520b9f5f9f21cebbc3b
Ike <ikesnowy@outlook.com>
2017-05-30 14:15:28 +0800

    1.1.3 & 1.1.4

---
+++ 1.1.3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._3
+{
+    /*
+     * 1.1.3 编写一个程序，从命令行获取三个整数参数。
+     * 如果它们都相等则打印 equal，
+     * 否则打印 not equal。
+     */
+    class Program
+    {
+        /*
+         * 输入
+         * 
+         * 用空格隔开的三个整数，例如
+         * 3 3 3
+         * 
+         * 输出
+         * 
+         * equal 或 not equal
+         */
+        static void Main(string[] args)
+        {
+            //Console.ReadLine() 从控制台读入一整行（返回int）
+            //string.Split(char) 根据提供的分隔符将字符串分割，返回字符串数组
+            //Int32.Parse(string) 将字符串转换为相应的整型数据
+            string input = Console.ReadLine();
+            int a = Int32.Parse(input.Split(' ')[0]);
+            int b = Int32.Parse(input.Split(' ')[1]);
+            int c = Int32.Parse(input.Split(' ')[2]);
+
+            //Console.WriteLine() 向控制台输出一行
+            if (a == b && b == c)
+            {
+                Console.WriteLine("equal");
+            }
+            else
+            {
+                Console.WriteLine("not equal");
+            }
+        }
+    }
+}
---
+++ 1.1.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._4
+{
+    /*
+     * 1.1.4 下列语句各有什么问题（如果有的话）？
+     * a. if (a > b) then c = 0;
+     * b. if a > b { c = 0; }
+     * c. if (a > b) c = 0;
+     * d. if (a > b) c = 0 else b = 0;
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 0;
+
+            //if (a > b) then c = 0; 
+            //if 后不能跟 then
+
+            //if a > b { c = 0; } 
+            //if后必须跟括号
+
+            if (a > b) c = 0; 
+            //正确
+
+            //if (a > b) c = 0 else b = 0; 
+            //c = 0后缺少分号
+
+        }
+    }
+}

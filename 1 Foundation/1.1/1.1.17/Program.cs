0413adcd7645674acc95e0ea614d5e422cfb3eb3
Ike <ikesnowy@outlook.com>
2017-05-31 13:31:42 +0800

    1.1.17

---
+++ 1 Foundation/1.1/1.1.17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._1._17
+{
+    /*
+     * 1.1.17
+     * 
+     * 找出以下递归函数的问题
+     * 
+     */ 
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{exR2(6)}");//输出 StackOverflow Exception
+        }
+
+        public static string exR2(int n)
+        {
+            string s = exR2(n - 3) + n + exR2(n - 2) + n;//运行到 exR2 即展开，不会再运行下一句
+            if (n <= 0) return "";
+            return s;
+        }
+    }
+}

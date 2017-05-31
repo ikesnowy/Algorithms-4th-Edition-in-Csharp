6aa6d36f93c05b96b8c91b9f91653168772b9584
Ike <ikesnowy@outlook.com>
2017-05-31 13:26:11 +0800

    1.1.16

---
+++ 1 Foundation/1.1/1.1.16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1._1._16
+{
+    /*
+     * 1.1.16
+     * 
+     * 给出 exR1(6) 的返回值
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{exR1(6)}");
+        }
+
+        //exR1(6) = 
+        //exR1(3) + 6 + exR1(4) + 6
+        //exR1(0) + 3 + exR1(1) + 3 + 6 + exR1(4) + 6
+        //"" + 3 + exR1(-2) + 1 + exR1(-1) + 1 + 3 + 6 + exR1(4) + 6
+        //"" + 3 + "" + 1 + "" + 1 + 3 + 6 + exR1(4) + 6
+        //"31136" + exR1(4) + 6
+        //......
+
+        public static string exR1(int n)
+        {
+            if (n <= 0)
+            {
+                return "";
+            }
+
+            return exR1(n - 3) + n + exR1(n - 2) + n;
+        }
+    }
+}

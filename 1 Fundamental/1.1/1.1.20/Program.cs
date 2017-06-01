ba7fd046a48676b8f8a359baf9bce8eaf4114729
Ike <ikesnowy@outlook.com>
2017-06-01 13:22:36 +0800

    1.1.20 Finished

---
+++ 1 Fundamental/1.1/1.1.20/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1._1._20
+{
+    /*
+     * 1.1.20
+     * 
+     * 编写一个递归的静态方法计算 ln(N!) 的值。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 4;
+            Console.WriteLine($"{factorialLn(N)}");
+        }
+
+        //ln(N!) =
+        //ln(N * (N - 1) * ... * 1) =
+        //ln(N) + ln((N - 1)!)
+        public static double factorialLn(int N)
+        {
+            if (N == 1)
+            {
+                return 0;
+            }
+
+            return Math.Log(N) + factorialLn(N - 1);
+        }
+    }
+}

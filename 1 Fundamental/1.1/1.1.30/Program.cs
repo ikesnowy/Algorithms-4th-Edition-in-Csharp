460976706f6c0ad50bc917088e497ea6d6ec7a53
Ike <ikesnowy@outlook.com>
2017-06-05 15:13:07 +0800

    1.1.30 Finished

---
+++ 1 Fundamental/1.1/1.1.30/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _1._1._30
+{
+    /*
+     * 1.1.30
+     * 
+     * 数组练习。
+     * 编写一段程序，创建一个 N×N 的布尔数组 a[][]。
+     * 其中当 i 和 j 互质时（没有相同的因子），a[i][j] 为 true，否则为 false。
+     * 
+     */
+    class Program
+    {
+        //互质 = 最大公约数为 1 = gcd(i, j) == 1
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            bool[,] a = new bool[N, N];
+
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j  = 0; j < N; ++j)
+                {
+                    a[i, j] = (gcd(i, j) == 1);
+                }
+            }
+
+            PrintArray2D(a, N, N);
+        }
+
+        static int gcd(int a, int b)
+        {
+            if (b == 0)
+                return a;
+
+            return gcd(b, a % b);
+        }
+
+        private static void PrintArray2D(bool[,] array, int rows, int columns)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"\t{array[i, j]}");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}

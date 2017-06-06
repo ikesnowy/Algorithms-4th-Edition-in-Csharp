b09f2dc0be163714092bf544f823ff06b24593f5
Ike <ikesnowy@outlook.com>
2017-06-06 22:42:31 +0800

    1.1.31 清除不必要的using & 添加题目描述

--- 1 Fundamental/1.1/1.1.31/Program.cs
+++ 1 Fundamental/1.1/1.1.31/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _1._1._31
 {
+    /*
+     * 1.1.31
+     * 
+     * 随机连接。
+     * 编写一段程序，从命令行接受一个整数 N 和 double 值 p （0 到 1 之间）作为参数，
+     * 在一个圆上画出大小为 0.05 且间距相等的 N 个点，
+     * 然后将每对点按照概率 p 用灰线连接。
+     * 
+     */
     static class Program
     {
         /// <summary>

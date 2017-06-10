ce42613e46fa0bf3d1947afd851dc7c39f897fbd
Ike <ikesnowy@outlook.com>
2017-06-10 22:25:09 +0800

    1.1.33 Dot operation

---
+++ 1 Fundamental/1.1/1.1.33/Matrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._1._33
+{
+    public class Matrix
+    {
+        /// <summary>
+        /// 向量点乘
+        /// </summary>
+        /// <param name="x">需要点乘的向量</param>
+        /// <param name="y">需要点乘的另一个向量</param>
+        /// <returns>返回点乘的结果</returns>
+        static double dot(double[] x, double[] y)
+        {
+            if (x.Length != y.Length)
+            {
+                throw new FormatException("向量不等长");
+            }
+
+            double result = 0;
+            for (int i = 0; i < x.Length; ++i)
+            {
+                result += x[i] * y[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 1 Fundamental/1.1/1.1.33/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._1._33
+{
+    /*
+     * 1.1.33
+     * 
+     * 矩阵库
+     * 编写一个矩阵库并实现下列 API
+     * 
+     * public class Matrix
+     *  static double dot(double[] x, double[] y)           //向量点乘
+     *  static double[][] mult(double[][] a, double[][] b)  //矩阵与矩阵之积
+     *  static double[][] transpose(double[][] a)           //矩阵转置
+     *  static double[] mult(double[][] a, double[] x)      //矩阵和向量之积
+     *  static double[] mult(double[] y, double[][] a)      //向量和矩阵之积
+     *  
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

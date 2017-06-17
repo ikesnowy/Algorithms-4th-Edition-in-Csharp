de795899d86c1b96dbdab406dcaca9d2ce6c6533
Ike <ikesnowy@outlook.com>
2017-06-17 21:05:37 +0800

    1.2.3 Started

---
+++ 1 Fundamental/1.2/1.2.3/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _1._2._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}

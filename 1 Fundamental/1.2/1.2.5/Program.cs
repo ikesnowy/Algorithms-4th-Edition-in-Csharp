406e2e0d2ddab7b3be5d768b733f12cd6270aef0
Ike <ikesnowy@outlook.com>
2017-06-19 13:24:29 +0800

    1.2.5

---
+++ 1 Fundamental/1.2/1.2.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._5
+{
+    /*
+     * 1.2.5
+     * 
+     * 以下这段代码会打印出什么？
+     * String s = "Hello World";
+     * s.toUpperCase();
+     * s.substring(6, 11);
+     * StdOut.println(s);
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World";
+            s.ToUpper();
+            s.Substring(6, 5);//C# 中两个参数分别代表子串起始下标和长度
+            Console.WriteLine(s);
+        }
+    }
+}

df9eb55c82868318a502240f0c0b67d3a46d8041
Ike <ikesnowy@outlook.com>
2017-06-19 13:19:03 +0800

    1.2.4 Finished

---
+++ 1 Fundamental/1.2/1.2.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._4
+{
+    /*
+     * 1.2.4
+     * 
+     * 以下这段代码会打印出什么？
+     * String string1 = "hello";
+     * String string2 = string1;
+     * string1 = "world";
+     * StdOut.println(string1);
+     * StdOut.println(string2);
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "hello";
+            string string2 = string1;
+            string1 = "world";
+            Console.WriteLine(string1);
+            Console.WriteLine(string2);
+        }
+    }
+}

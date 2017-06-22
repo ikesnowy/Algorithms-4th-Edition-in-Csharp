6cc229d82c4b9d1376f0061c9ffe1dbd7854800a
Ike <ikesnowy@outlook.com>
2017-06-22 19:30:23 +0800

    1.2.11 Finished

---
+++ 1 Fundamental/1.2/1.2.11/Date.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._11
+{
+    /// <summary>
+    /// 日期类，保存一个日期。
+    /// </summary>
+    class Date
+    {
+        public int Month { get; }//月
+        public int Day { get; }//日
+        public int Year { get; }//年
+
+        public Date(int m, int d, int y)
+        {
+            this.Month = m;
+            this.Day = d;
+            this.Year = y;
+        }
+
+        public override string ToString()
+        {
+            return this.Month + "/" + this.Day + "/" + this.Year;
+        }
+    }
+}
---
+++ 1 Fundamental/1.2/1.2.11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._11
+{
+    /*
+     * 1.2.11
+     * 
+     * 根据 Date 的 API 实现一个 SmartDate 类型，
+     * 在日期非法时抛出一个异常。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d = new Date(2, 29, 2017);
+            Console.WriteLine(d);
+
+            SmartDate sd = new SmartDate(2, 29, 2017);//抛出异常
+            Console.WriteLine(sd);
+        }
+    }
+}

e86169f9c3d525fa150fc25998f05c9f7ae1096d
Ike <ikesnowy@outlook.com>
2017-06-23 20:04:19 +0800

    1.2.12 Finished

---
+++ 1 Fundamental/1.2/1.2.12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._12
+{
+    /*
+     * 1.2.12
+     * 
+     * 为 SmartDate 添加一个方法 dayOfTheWeek()，
+     * 为日期中每周的日返回 Monday、Tuesday、Wednesday、Thursday、Friday、Saturday 或 Sunday 中的适当值。
+     * 你可以假定时间是 21 世纪。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartDate Today = new SmartDate(DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Year);
+            Console.WriteLine(Today.DayOfTheWeek());
+        }
+    }
+}

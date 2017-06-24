d529c613800767ac2de7a86bed4e75d5b315fbb8
Ike <ikesnowy@outlook.com>
2017-06-24 19:46:23 +0800

    1.2.13 Started

---
+++ 1 Fundamental/1.2/1.2.13/Date.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._13
+{
+    class Date : IComparable<Date>
+    {
+        public int Month { get; }//月
+        public int Day { get; }//日
+        public int Year { get; }//年
+
+        public Date(int m, int d, int y)
+        {
+            this.Month = m;
+            this.Day = d;
+            this.Year = y;
+        }
+
+        public override string ToString()
+        {
+            return this.Month + "/" + this.Day + "/" + this.Year;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+                return true;
+            if (obj == null)
+                return false;
+            if (obj.GetType() != this.GetType())
+                return false;
+            Date that = (Date)obj;
+            return (this.Year == that.Year) && (this.Month == that.Month) && (this.Day == that.Day);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = 31 * hash + this.Month;
+            hash = 31 * hash + this.Year;
+            hash = 31 * hash + this.Day;
+            return hash;
+        }
+
+        int IComparable<Date>.CompareTo(Date other)
+        {
+            if (this.Year > other.Year)
+                return 1;
+            else if (this.Year < other.Year)
+                return -1;
+
+            if (this.Month > other.Month)
+                return 1;
+            else if (this.Month < other.Month)
+                return -1;
+
+            if (this.Day > other.Day)
+                return 1;
+            else if (this.Day < other.Day)
+                return -1;
+
+            return 0;
+        }
+    }
+}
---
+++ 1 Fundamental/1.2/1.2.13/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._13
+{
+    class Transaction : IComparable<Transaction>
+    {
+        public string Who { get; }
+        public Date When { get; }
+        public double Amount { get; }
+
+        Transaction(string who, Date when, double amount)
+        {
+            this.Who = who;
+            this.When = when;
+            this.Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return 
+        }
+    }
+}

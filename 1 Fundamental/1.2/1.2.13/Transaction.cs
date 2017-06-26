660d1d818acaaaa18cccb93c68d37083118b8469
Ike <ikesnowy@outlook.com>
2017-06-26 10:31:21 +0800

    Transaction Finished

--- 1 Fundamental/1.2/1.2.13/Transaction.cs
+++ 1 Fundamental/1.2/1.2.13/Transaction.cs
@@ -14,6 +14,10 @@ namespace _1._2._13
 
         Transaction(string who, Date when, double amount)
         {
+            if (double.IsNaN(amount) || double.IsInfinity(amount))
+            {
+                throw new ArgumentException("Amount cannot be NaN or Infinity");
+            }
             this.Who = who;
             this.When = when;
             this.Amount = amount;
@@ -21,12 +25,41 @@ namespace _1._2._13
 
         public override string ToString()
         {
-            return 
+            return string.Format("%-10s %10s %8.2f", Who, When, Amount);
         }
 
         int IComparable<Transaction>.CompareTo(Transaction other)
         {
-            throw new NotImplementedException();
+            if (this.Amount < other.Amount)
+                return -1;
+            if (this.Amount > other.Amount)
+                return 1;
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+                return true;
+            if (obj == null)
+                return false;
+            if (obj.GetType() != this.GetType())
+                return false;
+            Transaction that = (Transaction)obj;
+
+            return
+                (that.Amount == this.Amount) &&
+                (that.When == this.When) &&
+                (that.Who == this.Who);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 1;
+            hash = 31 * hash + Who.GetHashCode();
+            hash = 31 * hash + When.GetHashCode();
+            hash = 31 * hash + Amount.GetHashCode();
+            return hash;
         }
     }
 }

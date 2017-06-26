ea5ea4f6ae7c966e96078096e020c26f12ecd9e7
Ike <ikesnowy@outlook.com>
2017-06-26 13:34:09 +0800

    1.2.15 Finished

---
+++ 1 Fundamental/1.2/1.2.15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._15
+{
+    /*
+     * 1.2.15
+     * 
+     * 文件输入。
+     * 基于 String 的 split() 方法实现 In 中的静态方法 readInts()。
+     * 
+     */
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tinyT = ReadInts("tinyT.txt");
+            foreach (int n in tinyT) 
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        public static int[] ReadInts(string path)
+        {
+            string[] allLines = File.ReadAllLines(path);
+            int[] result = new int[allLines.Length];
+            
+            for (int i = 0; i < allLines.Length; ++i)
+            {
+                result[i] = int.Parse(allLines[i]);
+            }
+
+            return result;
+        }
+    }
+}

353dea22f126280aa144c3a2f7c0ba1c60b2e4dc
Ike <ikesnowy@outlook.com>
2017-06-27 18:47:56 +0800

    1.2.18 Finished

---
+++ 1 Fundamental/1.2/1.2.18/Accumulator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._18
+{
+    public class Accumulator
+    {
+        private double m;
+        private double s;
+        private int N;
+
+        public void AddDataValue(double x)
+        {
+            N++;
+            s = s + 1.0 * (N - 1) / N * (x - m) * (x - m);
+            m = m + (x - m) / N;
+        }
+        public double Mean()
+        {
+            return m;
+        }
+        public double Var()
+        {
+            return s / (N - 1);
+        }
+        public double Stddev()
+        {
+            return Math.Sqrt(this.Var());
+        }
+        public override string ToString()
+        {
+            return "Mean (" + N + " values): " + string.Format("{0, 7:F5}", Mean());
+        }
+    }
+}
---
+++ 1 Fundamental/1.2/1.2.18/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._18
+{
+    /*
+     * 1.2.18
+     * 
+     * 累加器的方法。
+     * 以下代码为 Accumulator 类添加了 var() 和 stddev() 方法，
+     * 它们计算了 addDataValue() 方法的参数的方差和标准差，验证这段代码：
+     * 与直接对所有数据的平方求和的方法相比较，
+     * 这种实现能够更好的避免四舍五入产生的误差。
+     * 
+     */
+    class Program
+    {
+        //当数据比较大时—— 例如 10^9 加上随机小数组成的数列，这时 double 的小数精度将受限。
+        //求和之后整数部分更大，小数部分将自动四舍五入，出现误差
+        //这时再计算平均值时将会带来较大的误差。
+        //因此采用另一个递推公式：
+        //k 为下标。
+        //Mk = Mk-1+ (xk – Mk-1)/k
+        //Sk = Sk-1 + (xk – Mk-1)*(xk – Mk).
+        //方差 s^2 = Sk/(k – 1).
+        //这种情况下并没有直接对所有输入值求和，小数精度不受到整数部分长度的影响。
+        static void Main(string[] args)
+        {
+            int T = 100000;
+            Random random = new Random();
+            Accumulator a = new Accumulator();
+            for (int t = 0; t < T; ++t)
+            {
+                a.AddDataValue(random.NextDouble() + 1000000000);
+            }
+
+            Console.WriteLine(a.Stddev());
+            Console.WriteLine(a);
+        }
+    }
+}

22707823ed3573d82ce4d3071c089a5191a7f12f
Ike <ikesnowy@outlook.com>
2017-06-27 11:17:23 +0800

    1.2.17

---
+++ 1 Fundamental/1.2/1.2.17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._17
+{
+    /*
+     * 1.2.17
+     * 
+     * 有理数实现的健壮性。
+     * 在 Rational （请见练习 1.2.16）的开发中使用断言来防止溢出。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational a = new Rational(long.MaxValue, 1);
+            Rational b = new Rational(1, 1);
+            Rational c = a.Plus(b);
+        }
+    }
+}

3bcb08c2df8dd8442b3a765871deeb0db672b6a7
Ike <ikesnowy@outlook.com>
2017-06-29 19:02:21 +0800

    1.3.1 Finished

---
+++ 1 Fundamental/1.3/1.3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics;
+
+namespace _1._3._1
+{
+    /*
+     * 1.3.1
+     * 
+     * 为 FixedCapacityStackOfStrings 添加一个方法 isFull()。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FixedCapacityStackOfStrings fixedStack = new FixedCapacityStackOfStrings(2);
+
+            fixedStack.Push("lol");
+            Console.WriteLine(fixedStack.IsFull()); //false
+            fixedStack.Push("lol2");
+            Console.WriteLine(fixedStack.IsFull()); //true
+        }
+    }
+}

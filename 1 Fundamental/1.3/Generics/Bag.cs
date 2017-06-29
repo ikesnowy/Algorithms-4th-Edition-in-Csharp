947268e1d7bacb3f15d6d755bbcc4cb76d542393
Ike <ikesnowy@outlook.com>
2017-06-29 09:50:19 +0800

    Queue & Bag & Stack Finished

---
+++ 1 Fundamental/1.3/Generics/Bag.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Bag<Item> : IEnumerable<Item>
+    {
+        private Node<Item> first;
+        private int count;
+
+        /// <summary>
+        /// 默认构造函数。
+        /// </summary>
+        public Bag()
+        {
+            first = null;
+            count = 0;
+        }
+
+        /// <summary>
+        /// 背包是否为空。
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+
+        /// <summary>
+        /// 返回背包中元素的数量。
+        /// </summary>
+        /// <returns></returns>
+        public int Size()
+        {
+            return count;
+        }
+
+        /// <summary>
+        /// 向背包中添加一个元素。
+        /// </summary>
+        /// <param name="item">要添加的元素。</param>
+        public void Add(Item item)
+        {
+            Node<Item> oldFirst = first;
+            first = new Node<Item>();
+            first.item = item;
+            first.next = oldFirst;
+            count++;
+        }
+
+        public IEnumerator<Item> GetEnumerator()
+        {
+            return new BagEnumerator(this.first);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public class BagEnumerator : IEnumerator<Item>
+        {
+            private Node<Item> current;
+            private Node<Item> first;
+
+            Item IEnumerator<Item>.Current => current.item;
+
+            object IEnumerator.Current => current.item;
+
+            public BagEnumerator(Node<Item> first)
+            {
+                this.current = new Node<Item>();
+                this.current.next = first;
+                this.first = current;
+            }
+
+            void IDisposable.Dispose()
+            {
+                this.current = null;
+                this.first = null;
+                return;
+            }
+
+            bool IEnumerator.MoveNext()
+            {
+                if (this.current.next == null)
+                    return false;
+
+                Item item = this.current.item;
+                this.current = this.current.next;
+                return true;
+            }
+
+            void IEnumerator.Reset()
+            {
+                this.current = this.first;
+            }
+        }
+    }
+
+
+}
---
+++ 1 Fundamental/1.3/Generics/Stack.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Stack<Item> : IEnumerable<Item>
+    {
+        private Node<Item> first;
+        private int count;
+
+        public Stack()
+        {
+            this.first = null;
+            this.count = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.first == null;
+        }
+
+        public int Size()
+        {
+            return this.count;
+        }
+
+        public void Push(Item item)
+        {
+            Node<Item> oldFirst = first;
+            this.first = new Node<Item>();
+            this.first.item = item;
+            this.first.next = oldFirst;
+            count++;
+        }
+
+        public Item Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack Underflow");
+            Item item = first.item;
+            first = first.next;
+            count--;
+            return item;
+        }
+
+        public Item Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack Underflow");
+            return first.item;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            foreach (Item n in this)
+            {
+                s.Append(n);
+                s.Append(' ');
+            }
+            return s.ToString();
+        }
+
+        public IEnumerator<Item> GetEnumerator()
+        {
+            return new StackEnumerator(this.first);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class StackEnumerator : IEnumerator<Item>
+        {
+            private Node<Item> current;
+            private Node<Item> first;
+
+            public StackEnumerator(Node<Item> first)
+            {
+                this.current = new Node<Item>();
+                this.current.next = first;
+                this.first = this.current;
+            }
+
+            Item IEnumerator<Item>.Current => current.item;
+
+            object IEnumerator.Current => current.item;
+
+            void IDisposable.Dispose()
+            {
+                this.current = null;
+                this.first = null;
+            }
+
+            bool IEnumerator.MoveNext()
+            {
+                if (this.current.next == null)
+                    return false;
+
+                this.current = this.current.next;
+                return true;
+            }
+
+            void IEnumerator.Reset()
+            {
+                this.current = this.first;
+            }
+        }
+    }
+}

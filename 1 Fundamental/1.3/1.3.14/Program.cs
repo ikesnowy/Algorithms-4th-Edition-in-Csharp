e2c82e78abf826a57707891ad2bc562cdaecc439
Ike <ikesnowy@outlook.com>
2017-07-03 20:56:57 +0800

    1.3.14 Finished

---
+++ 1 Fundamental/1.3/1.3.14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3._14
+{
+    /*
+     * 1.3.14
+     * 
+     * 编写一个类 ResizingArrayQueueOfStrings，
+     * 使用定长数组实现队列的抽象，然后扩展实现，
+     * 使用调整数组的方法突破大小的限制。
+     * 
+     */
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ResizingArrayQueueOfStrings<string> queue = new ResizingArrayQueueOfStrings<string>();
+            string[] input = "to be or not to - be - - that - - - is".Split(' ');
+
+            foreach (string s in input)
+            {
+                if (!s.Equals("-"))
+                    queue.Enqueue(s);
+                else if (!queue.IsEmpty())
+                    Console.Write(queue.Dequeue() + ' ');
+            }
+            Console.WriteLine("(" + queue.Size() + " left on queue)");
+        }
+    }
+}

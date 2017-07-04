e63d6885bd2543b98475dea537c9f9e75413afcd
Ike <ikesnowy@outlook.com>
2017-07-04 09:39:19 +0800

    1.3.15 Finished

---
+++ 1 Fundamental/1.3/1.3.15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics;
+
+namespace _1._3._15
+{
+    /*
+     * 1.3.15
+     * 
+     * 编写一个 Queue 的用例，接受一个命令行参数 k 并打印出标准输入中的倒数第 k 个字符串
+     * （假设标准输入中至少有 k 个字符串）。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            string[] input = "1 2 3 4 5 6 7 8 9 10".Split(' ');
+            int k = 4;
+
+            foreach(string s in input)
+            {
+                queue.Enqueue(s);
+            }
+
+            int count = queue.Size() - k;
+            for(int i = 0; i < count; ++i)
+            {
+                queue.Dequeue();
+            }
+
+            Console.WriteLine(queue.Peek());
+        }
+    }
+}

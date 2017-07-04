9926d91d8da94e618ccbef58dde573ea97a58d7a
Ike <ikesnowy@outlook.com>
2017-07-04 10:34:42 +0800

    1.3.16 Finished

---
+++ 1 Fundamental/1.3/1.3.16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //输入结束后按 Ctrl + Z 标记结尾
+            //输入格式：06/30/2017
+            //以回车分隔
+            Date[] date = Date.ReadDates();
+            foreach (Date d in date)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}

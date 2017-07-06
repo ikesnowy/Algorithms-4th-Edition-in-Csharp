1dee6ea05b4534d49212f5b3e803af6285f986ef
Ike <ikesnowy@outlook.com>
2017-07-06 15:42:12 +0800

    1.3.31 Finished

---
+++ 1 Fundamental/1.3/1.3.31/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3._31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkList<string> linklist = new DoubleLinkList<string>();
+            linklist.InsertRear("fourth");
+            linklist.InsertFront("first");
+            linklist.InsertAfter("second", 0);
+            linklist.InsertBefore("third", 2);
+
+            Console.WriteLine(linklist);
+
+            linklist.DeleteFront();
+            Console.WriteLine(linklist);
+            linklist.DeleteRear();
+            Console.WriteLine(linklist);
+            linklist.Delete(1);
+            Console.WriteLine(linklist);
+
+            Console.WriteLine(linklist.At(0)); 
+        }
+    }
+}

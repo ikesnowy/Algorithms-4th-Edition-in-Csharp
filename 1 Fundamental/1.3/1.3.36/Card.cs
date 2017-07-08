8ba3a9359c52531f985508af950331f2699dbcf9
Ike <ikesnowy@outlook.com>
2017-07-08 12:42:07 +0800

    1.3.36 Finished

---
+++ 1 Fundamental/1.3/1.3.36/Card.cs
@@ -0,0 +1,49 @@
+﻿namespace _1._3._36
+{
+    class Card
+    {
+        Suit suit;
+        int number;
+
+        public Card(Suit suit, int number)
+        {
+            this.suit = suit;
+            this.number = number;
+        }
+
+        public override string ToString()
+        {
+            string num;
+            if (this.number == 1)
+            {
+                num = "Ace";
+            }
+            else if (this.number == 11)
+            {
+                num = "Jack";
+            }
+            else if (this.number == 12)
+            {
+                num = "Queen";
+            }
+            else if (this.number == 13)
+            {
+                num = "King";
+            }
+            else
+            {
+                num = this.number.ToString();
+            }
+
+            return num + " of the " + suit.ToString();
+        }
+    }
+
+    enum Suit
+    {
+        Clubs = 0,
+        Diamonds = 1,
+        Hearts = 2,
+        Spades = 3
+    };
+}

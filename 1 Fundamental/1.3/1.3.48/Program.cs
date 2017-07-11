99b329b32a5571252a863be7d98e7fdc06cf3ca6
Ike <ikesnowy@outlook.com>
2017-07-11 20:41:04 +0800

    1.3.48 Finished Fixed a bug in Deque

--- 1 Fundamental/1.3/1.3.48/Program.cs
+++ 1 Fundamental/1.3/1.3.48/Program.cs
@@ -18,7 +18,25 @@ namespace _1._3._48
     {
         static void Main(string[] args)
         {
+            DeStack<string> destack = new DeStack<string>();
+            string input = "to be or not to - be - - that - - - is";
+            string[] s = input.Split(' ');
 
+            foreach (string n in s)
+            {
+                if (!n.Equals("-"))
+                    destack.PushRight(n);
+                else if (!destack.IsRightEmpty())
+                    Console.WriteLine(destack.PopRight());
+            }
+
+            foreach (string n in s)
+            {
+                if (!n.Equals("-"))
+                    destack.PushLeft(n);
+                else if (!destack.IsLeftEmpty())
+                    Console.WriteLine(destack.PopLeft());
+            }
         }
     }
 }

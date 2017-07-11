f2be4a77b5c595b7a83f737797c8ef5ac5cf7665
Ike <ikesnowy@outlook.com>
2017-07-11 20:21:49 +0800

    1.3.49 HalfDone

---
+++ 1 Fundamental/1.3/1.3.49/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3._49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "to be or not to - be - - that - - - is";
+            string[] s = input.Split(' ');
+            StackQueue<string> queue = new StackQueue<string>();
+
+            foreach (string n in s)
+            {
+                if (!n.Equals("-"))
+                    queue.Enqueue(n);
+                else
+                    Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}

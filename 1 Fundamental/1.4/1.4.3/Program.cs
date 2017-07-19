aef003fa2cd5a3a0c2b902ad66e29eb063faf683
Ike <ikesnowy@outlook.com>
2017-07-19 21:37:44 +0800

    1.4.3 halfdone

--- 1 Fundamental/1.4/1.4.3/Program.cs
+++ 1 Fundamental/1.4/1.4.3/Program.cs
@@ -22,16 +22,22 @@ namespace _1._4._3
 
         public static void PaintLinear()
         {
+            //新建一个绘图窗口
             Form2 linear = new Form2();
             linear.Show();
+            //新建画布
+            Graphics canvas = linear.CreateGraphics();
+            //获取窗口区域
             Rectangle rect = linear.ClientRectangle;
+            //计算单位长度（十等分）
             int unitY = rect.Height / 10;
             int unitX = rect.Width / 10;
+            //获取中心区域（上下左右增加 10% 的内补）
             Rectangle center = new Rectangle(rect.X + unitX, rect.Y + unitY, unitX * 8, unitY * 8);
-            Graphics canvas = linear.CreateGraphics();
+            //绘制坐标系
             canvas.DrawLine(Pens.Black, center.X, center.Y, center.X, center.Y + center.Height);
             canvas.DrawLine(Pens.Black, center.X, center.Y + center.Height, center.X + center.Width, center.Y + center.Height);
-
+            canvas.DrawString("N", linear.Font, Brushes.Black, center.X, center.Y + center.Height);
 
         }
     }

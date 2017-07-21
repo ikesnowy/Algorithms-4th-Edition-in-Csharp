27a1e2bbc85604e9873c4c9b112768aed123dffb
Ike <ikesnowy@outlook.com>
2017-07-21 10:32:07 +0800

    1.4.4 Finished

---
+++ 1 Fundamental/1.4/1.4.4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4._4
+{
+    /*
+     * 1.4.4
+     * 
+     * 参照表 1.4.4 为 TwoSum 建立一张类似的表格。
+     * 
+     */
+    class Program
+    {
+        public static int Count(int[] a)
+        { //A                                                                               
+            int n = a.Length;
+            int count = 0;
+            for (int i = 0; i < n; ++i)
+            {//B
+                for (int j = i + 1; j < n; ++j)
+                {//C
+                    if (a[i] + a[j] == 0)
+                    {
+                        count++;//D
+                    }
+                }
+            }
+            return count;
+        }
+
+        //语句块   单次执行时间    频率             总时间
+        //  D        t0        x（输入相关）        xt0
+        //  C        t1        N^2/2 - N/2         t1(N^2/2 - N/2)
+        //  B        t2            N               t2N
+        //  A        t3            1               t3
+        //
+        //总计
+        //(N^2/2)t1
+        //  + N(t2 - t1/2)
+        //      + t3 + xt0
+        //约为（当 x 很小时）
+        //(N^2/2)t1
+        //增长的数量级
+        //N^2
+    }
+}
---
+++ 1 Fundamental/1.4/Measurement/TwoSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Measurement
+{
+    public static class TwoSum
+    {
+        /// <summary>
+        /// 打印出数组中所有和为零的整数对。
+        /// </summary>
+        /// <param name="a">查找范围。</param>
+        public static void PrintAll(int[] a)
+        {
+            int n = a.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = i + 1; j < n; ++j)
+                {
+                    if (a[i] + a[j] == 0)
+                    {
+                        Console.WriteLine(a[i] + " " + a[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计算数组中和为零的整数对数量。
+        /// </summary>
+        /// <param name="a">查找范围。</param>
+        /// <returns></returns>
+        public static int Count(int[] a)
+        {
+            int n = a.Length;
+            int count = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = i + 1; j < n; ++j)
+                {
+                    if (a[i] + a[j] == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}

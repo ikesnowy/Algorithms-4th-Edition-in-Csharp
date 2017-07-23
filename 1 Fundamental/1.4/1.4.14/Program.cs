0f08773087097f8da93db351fe75f69d8a2222f0
Ike <ikesnowy@outlook.com>
2017-07-23 10:54:51 +0800

    1.4.14 Finished

---
+++ 1 Fundamental/1.4/1.4.14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Measurement;
+
+namespace _1._4._14
+{
+    /*
+     * 1.4.14
+     * 
+     * 4-sum。
+     * 为 4-sum 设计一个算法。
+     * 
+     */
+    class Program
+    {
+        //见 Measurement.FourSum 类
+        static void Main(string[] args)
+        {
+            //char[] split = new char[1] { '\n' };
+            //string[] testcases = TestCase.Properties.Resources._1Kints.Split(split, StringSplitOptions.RemoveEmptyEntries);
+            //long[] a = new long[testcases.Length];
+            //for (int i = 0; i < testcases.Length; ++i)
+            //{
+            //    a[i] = long.Parse(testcases[i]);
+            //}
+
+            long[] a = new long[20] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
+
+            Stopwatch timer = new Stopwatch();
+            FourSum.PrintAll(a);
+            Console.WriteLine(timer.ElapsedTime());
+        }
+    }
+}

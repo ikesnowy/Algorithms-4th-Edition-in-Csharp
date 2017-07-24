71fef64b3d51cce8de2fee86ab80c6fecd41bee5
Ike <ikesnowy@outlook.com>
2017-07-24 10:32:26 +0800

    1.4.15 Finished

--- 1 Fundamental/1.4/1.4.15/Program.cs
+++ 1 Fundamental/1.4/1.4.15/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Measurement;
 
 namespace _1._4._15
 {
@@ -18,8 +19,83 @@ namespace _1._4._15
      */
     class Program
     {
+        //由于数组已经排序（从小到大），负数在左侧，正数在右侧。
+        //TwoSumFaster
+        //设最左侧下标为 lo，最右侧下标为 hi。
+        //如果 a[lo] + a[hi] > 0, 说明正数太大，hi--。
+        //如果 a[lo] + a[hi] < 0，说明负数太小，lo++。
+        //否则就找到了一对和为零的整数对，lo++, hi--。
+        //
+        //ThreeSumFaster
+        //对于数组中的每一个数 a，ThreeSum 问题就等于求剩余数组中所有和为 -a 的 TwoSum 问题。
+        //只要在 TwoSumFaster 外层再套一个循环即可。
         static void Main(string[] args)
         {
+            char[] split = new char[1] { '\n' };
+            string[] testCases = TestCase.Properties.Resources._1Kints.Split(split, StringSplitOptions.RemoveEmptyEntries);
+            int[] a = new int[testCases.Length];
+            for (int i = 0; i < testCases.Length; ++i)
+            {
+                a[i] = int.Parse(testCases[i]);
+            }
+            Array.Sort(a);
+            Console.WriteLine(TwoSum.Count(a));
+            Console.WriteLine(TwoSumFaster(a));
+            Console.WriteLine(ThreeSum.Count(a));
+            Console.WriteLine(ThreeSumFaster(a));
+        }
+
+        static int TwoSumFaster(int[] a)
+        {
+            int i = 0;
+            int j = a.Length - 1;
+            int count = 0;
+            while (i != j)
+            {
+                if (a[i] + a[j] == 0)
+                {
+                    count++;
+                    i++;
+                    j--;
+                }
+                else if (a[i] + a[j] < 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            return count;
+        }
+
+        static int ThreeSumFaster(int[] a)
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length; ++i)
+            {
+                int lo = i + 1;
+                int hi = a.Length - 1;
+                while (lo <= hi)
+                {
+                    if (a[lo] + a[hi] + a[i] == 0)
+                    {
+                        count++;
+                        lo++;
+                        hi--;
+                    }
+                    else if (a[lo] + a[hi] + a[i] < 0)
+                    {
+                        lo++;
+                    }
+                    else
+                    {
+                        hi--;
+                    }
+                }
+            }
+            return count;
         }
     }
 }

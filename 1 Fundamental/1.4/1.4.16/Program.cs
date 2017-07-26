b2adb89b1ca6685a8deb0a5d66668f7c9f72b276
Ike <ikesnowy@outlook.com>
2017-07-26 10:22:55 +0800

    1.4.16 Finished

--- 1 Fundamental/1.4/1.4.16/Program.cs
+++ 1 Fundamental/1.4/1.4.16/Program.cs
@@ -17,11 +17,11 @@ namespace _1._4._16
      */
     class Program
     {
-        //总运行时间： logN + N = N
+        //总运行时间： NlogN + N = NlogN 
         static void Main(string[] args)
         {
             double[] a = new double[5] { 0.1, 0.3, 0.6, 0.8, 0 };
-            Array.Sort(a);//log(N)
+            Array.Sort(a);//Nlog(N) 具体见 https://msdn.microsoft.com/zh-cn/library/6tf1f0bc(v=vs.110).aspx 备注部分
             double minDiff = double.MaxValue;
             double minA = 0;
             double minB = 0;

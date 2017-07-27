22ff276a2c1e2c4dbfaa4b7f0ee427c38c066972
Ike <ikesnowy@outlook.com>
2017-07-27 22:20:44 +0800

    1.4.19 Started

---
+++ 1 Fundamental/1.4/1.4.19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4._19
+{
+    /*
+     * 1.4.19
+     * 
+     * 矩阵的局部最小元素。
+     * 给定一个含有 N^2 个不同整数的 N×N 数组 a[]。
+     * 设计一个运送时间和 N 成正比的算法来找出一个局部最小元素：
+     * 满足 a[i][j] < a[i+1][j]、a[i][j] < a[i][j+1]、a[i][j] < a[i-1][j] 以及 a[i][j] < a[i][j-1] 的索引 i 和 j。
+     * 程序运行时间在最坏情况下应该和 N 成正比。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

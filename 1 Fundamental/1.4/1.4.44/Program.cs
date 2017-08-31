a7e203908fda45acbba01a0fe719f24c88a4a81c
沈星繁 <ikesnowy@outlook.com>
2017-08-31 14:53:46 +0800

    1.4.44 Finished

---
+++ 1 Fundamental/1.4/1.4.44/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4._44
+{
+    /*
+     * 1.4.44
+     * 
+     * 生日问题。
+     * 编写一个程序，
+     * 从命令行接受一个整数 N 作为参数并使用 StdRandom.uniform() 生成一系列 0 到 N-1 之间的随机整数。
+     * 通过实验验证产生第一个重复的随机数之前生成的整数数量为 ~√(πN/2)。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int N = 10000;
+            int[] a = new int[N];
+            int dupNum = 0;
+            int times = 0;
+            for (times = 0; times < 500; ++times)
+            {
+                for (int i = 0; i < N; ++i)
+                {
+                    a[i] = random.Next(N);
+                    if (IsDuplicated(a, i))
+                    {
+                        dupNum += i;
+                        Console.WriteLine($"生成{i + 1}个数字后发生重复");
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"√(πN/2)={Math.Sqrt(Math.PI * N / 2.0)}，平均生成{dupNum / times}个数字后出现重复");
+        }
+
+        static bool IsDuplicated(int[] a, int i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (a[j] == a[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}

7f93c6686cd23bb4d96e678a6adedad0f7a86ae8
沈星繁 <ikesnowy@outlook.com>
2017-09-22 21:16:23 +0800

    1.5.7 Finished

---
+++ 1 Fundamental/1.5/1.5.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnionFind;
+
+namespace _1._5._7
+{
+    /*
+     * 1.5.7
+     * 
+     * 分别为 quick-find 算法和 quick-union 算法实现 QuickFindUF 类和 QuickUnionUF 类。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 查看 UnionFound 类库里面的相关类即可。
+            QuickFindUF quickFindUF = new QuickFindUF(100);
+            QuickUnionUF quickUnionUF = new QuickUnionUF(100);
+        }
+    }
+}

896cfa8d2db132d5bcdd60f59445456462ff4645
沈星繁 <ikesnowy@outlook.com>
2017-09-22 21:12:41 +0800

    1.5.6 Finished

---
+++ 1 Fundamental/1.5/1.5.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._5._6
+{
+    /*
+     * 1.5.6
+     * 
+     * 使用加权 quick-union 算法完成练习 1.5.5 。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 和上题类似的思路，
+             * 但是加权 quick-union 算法最多只需要 lgN 次迭代就可以完成一次 union()。
+             * 
+             * 因此按照上题思路，总共需要 (lg(10^9) * 10^6 * 10) / 10^9 = 0.299 秒。
+             * 
+             */
+        }
+    }
+}

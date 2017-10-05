f6f94649e7dfc64230c42f62f9e672a0b862049d
Ike <ikesnowy@outlook.com>
2017-10-05 14:31:11 +0800

    1.5.16 Finished.

---
+++ 1 Fundamental/1.5/1.5.16/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UnionFind;
+
+namespace _1._5._16
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}

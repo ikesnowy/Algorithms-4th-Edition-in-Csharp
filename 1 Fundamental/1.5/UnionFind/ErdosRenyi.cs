6e548068109723c73ebf1f57b0cc6e8b7af11723
Ike <ikesnowy@outlook.com>
2017-10-05 14:52:22 +0800

    1.5.17 Finished.

---
+++ 1 Fundamental/1.5/UnionFind/ErdosRenyi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnionFind
+{
+    public class ErdosRenyi
+    {
+        public static int Count(UF uf)
+        {
+            Random random = new Random();
+            int size = uf.Count();
+            int edges = 0;
+            while (uf.Count() > 1)
+            {
+                int p = random.Next(size);
+                int q = random.Next(size);
+                uf.Union(p, q);
+                edges++;
+            }
+
+            return edges;
+        }
+    }
+}

b71683b793dcb2e8ef9a66d93d04374562e7cfdc
Ike <ikesnowy@outlook.com>
2017-10-08 16:25:54 +0800

    1.1 Add space between "//" and comments.

--- 1 Fundamental/1.1/1.1.1/Program.cs
+++ 1 Fundamental/1.1/1.1.1/Program.cs
@@ -18,7 +18,7 @@ namespace _1._1._1
             double b = 2.0e-6 * 100000000.1;
             bool c = true && false || true && true;
 
-            //Console.WriteLine 向控制台窗口输出一行
+            // Console.WriteLine 向控制台窗口输出一行
             Console.WriteLine($"a.{a}");
             Console.WriteLine($"b.{b}");
             Console.WriteLine($"c.{c}");
--- 1 Fundamental/1.1/1.1.18/Program.cs
+++ 1 Fundamental/1.1/1.1.18/Program.cs
@@ -32,17 +32,17 @@ namespace _1._1._18
             Console.WriteLine($"mysteryChanged(3, 2): {mysteryChanged(3, 2)}");
         }
 
-        //mystery(a, b) = a * b
-        //利用等式：a * b = 2a * b/2 = (2a * (b-1) / 2) + a
-        //示例：
-        //mystery(2, 25) =
-        //mystery(2 + 2, 12) + 2 =
-        //mystery(4 + 4, 6) + 2 =
-        //mystery(8 + 8, 3) =
-        //mystery(16 + 16, 1) + 16 + 2 =
-        //mystery(32 + 32, 0) + 32 + 16 + 2 =
-        //0 + 32 + 16 + 2 =
-        //50
+        // mystery(a, b) = a * b
+        // 利用等式：a * b = 2a * b/2 = (2a * (b-1) / 2) + a
+        // 示例：
+        // mystery(2, 25) =
+        // mystery(2 + 2, 12) + 2 =
+        // mystery(4 + 4, 6) + 2 =
+        // mystery(8 + 8, 3) =
+        // mystery(16 + 16, 1) + 16 + 2 =
+        // mystery(32 + 32, 0) + 32 + 16 + 2 =
+        // 0 + 32 + 16 + 2 =
+        // 50
         public static int mystery(int a, int b)
         {
             if (b == 0) return 0;
@@ -50,9 +50,9 @@ namespace _1._1._18
             return mystery(a + a, b / 2) + a;
         }
 
-        //mysteryChanged(a, b) = a ^ b
-        //同理（乘方与乘法，乘法与加法之间具有类似的性质）
-        //a ^ b = (a ^ 2) ^ (b / 2) = (a ^ 2) ^ ((b - 1) / 2) * a
+        // mysteryChanged(a, b) = a ^ b
+        // 同理（乘方与乘法，乘法与加法之间具有类似的性质）
+        // a ^ b = (a ^ 2) ^ (b / 2) = (a ^ 2) ^ ((b - 1) / 2) * a
         public static int mysteryChanged(int a, int b)
         {
             if (b == 0) return 1;

2017c99bca760b9a20539d971985c9341f957c60
Ike <ikesnowy@outlook.com>
2017-11-26 20:22:36 +0800

    2.1.12 Finished.

---
+++ 2 Sorting/2.1/2.1.12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1._12
+{
+    /*
+     * 2.1.12
+     * 
+     * 令希尔排序打印出递增序列的每个元素所带来的比较次数和数组大小的比值。
+     * 编写一个测试用例对随机 Double 数组进行希尔排序，
+     * 验证该值是一个小常数，数组大小按照 10 的幂次递增，
+     * 不小于 100。
+     * 
+     */
+    class Program
+    {
+        // 查看最后结果
+        // 可以发现相同的 h 在数组大小不同时所产生的比值十分接近。
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            ShellSort sort = new ShellSort();
+
+            int size = 100;
+            for (int i = 0; i < 5; i++)
+            {
+                double[] a = new double[size];
+                for (int j = 0; j < size; j++)
+                {
+                    a[j] = random.NextDouble() * 100;
+                }
+                Console.WriteLine("ArraySize:" + size);
+                sort.Sort(a);
+                size *= 10;
+            }
+        }
+    }
+}
---
+++ 2 Sorting/2.1/2.1.12/ShellSort.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sort;
+
+namespace _2._1._12
+{
+    public class ShellSort : BaseSort
+    {
+        /// <summary>
+        /// 默认构造函数。
+        /// </summary>
+        public ShellSort() { }
+
+        /// <summary>
+        /// 利用希尔排序将数组按升序排序。
+        /// </summary>
+        /// <param name="a">需要排序的数组。</param>
+        public override void Sort<T>(T[] a)
+        {
+
+            int n = a.Length;
+
+            int h = 1;
+            while (h < n / 3)
+            {
+                h = 3 * h + 1;
+            }
+
+            while (h >= 1)
+            {
+                int compareTime = 0;
+                for (int i = h; i < n; i++)
+                {
+                    for (int j = i; j >= h && Less(a[j], a[j - h]); j -= h)
+                    {
+                        compareTime++;
+                        Exch(a, j, j - h);
+                    }
+                }
+                Debug.Assert(IsHSorted(a, h));
+                Console.Write("h:" + h + "\t");
+                Console.WriteLine("CompareTime/ArraySize = " + (double)compareTime / a.Length);
+                h /= 3;
+                
+            }
+            Debug.Assert(IsSorted(a));
+            
+        }
+
+        /// <summary>
+        /// 检查一次希尔排序后的子数组是否有序。
+        /// </summary>
+        /// <param name="a">排序后的数组。</param>
+        /// <param name="h">子数组间隔。</param>
+        /// <returns>是否有序。</returns>
+        private bool IsHSorted<T>(T[] a, int h) where T : IComparable<T>
+        {
+            for (int i = h; i < a.Length; i++)
+            {
+                if (Less(a[i], a[i - h]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}

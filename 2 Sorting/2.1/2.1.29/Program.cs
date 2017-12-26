76ce69b6eca2fd20b9460f9ea5ec08062e5d195d
沈星繁 <ikesnowy@outlook.com>
2017-12-26 14:38:33 +0800

    2.1.29 Finished

---
+++ 2 Sorting/2.1/2.1.29/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sort;
+using System.Diagnostics;
+
+namespace _2._1._29
+{
+    /*
+     * 2.1.29
+     * 
+     * 希尔排序的递增序列。
+     * 通过实验比较算法 2.3 中所使用的递增序列和
+     * 递增序列 1, 5, 19, 41, 109, 209, 505, 929, 2161, 3905, 8929, 16001, 36289, 64769, 146305, 260609 
+     * （这是通过序列 9×4^(k)-9×2^(k)+1 和 4^(k)-3×2^(k)+1 综合得到的）。
+     * 可以参考练习 2.1.11。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] h1 = { 1, 5, 19, 41, 109, 209, 505, 929, 2161, 3905, 8929, 16001, 36289, 64769, 146305, 260609 };
+            int[] h2 = new int[h1.Length];
+
+            int hTemp = 1;
+            for (int i = 0; i < h1.Length; i++)
+            {          
+                h2[i] = hTemp;
+                hTemp = hTemp * 3 + 1;
+            }
+
+            int n = 512;
+            ShellSort shellSort = new ShellSort();
+            Stopwatch timer = new Stopwatch();
+
+            int[] array1, array2;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("array size:" + n);
+                array1 = GetRandomInput(n);
+                array2 = new int[n];
+                array1.CopyTo(array2, 0);
+
+                Console.Write("\tnew sequence");
+                timer.Restart();
+                shellSort.Sort(array1, h1);
+                Console.WriteLine("\ttime:" + timer.ElapsedMilliseconds);
+
+                Console.Write("\torigin sequence");
+                timer.Restart();
+                shellSort.Sort(array2, h2);
+                Console.WriteLine("\ttime:" + timer.ElapsedMilliseconds);
+
+                n *= 2;
+            }
+        }
+
+        static int[] GetRandomInput(int n)
+        {
+            Random random = new Random();
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next();
+            }
+            return array;
+        }
+    }
+}
---
+++ 2 Sorting/2.1/2.1.29/ShellSort.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sort;
+
+namespace _2._1._29
+{
+    public class ShellSort : BaseSort
+    {
+        /// <summary>
+        /// 默认构造函数。
+        /// </summary>
+        public ShellSort() { }
+
+        /// <summary>
+        /// 利用希尔排序将数组按升序排序。
+        /// </summary>
+        /// <typeparam name="T">待排序的元素类型。</typeparam>
+        /// <param name="a">待排序的数组。</param>
+        /// <param name="h">需要使用的递增序列。</param>
+        public void Sort<T>(T[] a, int[] h) where T : IComparable<T>
+        {
+            int n = a.Length;
+            int t = 0;
+            while (h[t] < a.Length)
+            {
+                t++;
+                if (t >= h.Length)
+                    break;
+            }
+            t--;
+
+            for ( ; t >= 0; t--)
+            {
+                for (int i = h[t]; i < n; i++)
+                {
+                    for (int j = i; j >= h[t] && Less(a[j], a[j - h[t]]); j -= h[t])
+                    {
+                        Exch(a, j, j - h[t]);
+                    }
+                }
+                Debug.Assert(IsHSorted(a, h[t]));
+            }
+            Debug.Assert(IsSorted(a));
+        }
+
+        /// <summary>
+        /// 利用希尔排序将数组按升序排序。
+        /// </summary>
+        /// <param name="a">需要排序的数组。</param>
+        public override void Sort<T>(T[] a)
+        {
+            int n = a.Length;
+            int[] h = new int[2];   // 预先准备好的 h 值数组
+
+            int hTemp = 1;
+            for (int i = 0; hTemp < n; i++)
+            {
+                if (i >= h.Length)  // 如果数组不够大则双倍扩容
+                {
+                    int[] expand = new int[h.Length * 2];
+                    for (int j = 0; j < h.Length; j++)
+                    {
+                        expand[j] = h[j];
+                    }
+                    h = expand;
+                }
+                h[i] = hTemp;
+                hTemp = hTemp * 3 + 1;
+            }
+
+            for (int t = h.Length - 1; t >= 0; t--)
+            {
+                for (int i = h[t]; i < n; i++)
+                {
+                    for (int j = i; j >= h[t] && Less(a[j], a[j - h[t]]); j -= h[t])
+                    {
+                        Exch(a, j, j - h[t]);
+                    }
+                }
+                Debug.Assert(IsHSorted(a, h[t]));
+            }
+            Debug.Assert(IsSorted(a));
+        }
+
+        /// <summary>
+        /// 检查一次希尔排序后的子数组是否有序。
+        /// </summary>
+        /// <param name="a">排序后的数组。</param>
+        /// <param name="h">子数组间隔。</param>
+        /// <returns>是否有序。</returns>
+        private bool IsHSorted<T>(T[] a, int h) where T : IComparable<T>
+        {
+            for (int i = h; i < a.Length; i++)
+            {
+                if (Less(a[i], a[i - h]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}

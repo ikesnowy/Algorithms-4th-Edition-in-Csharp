605f412d7caf3d2cd10085dd3024faf057d19d59
沈星繁 <ikesnowy@outlook.com>
2018-02-01 18:45:39 +0800

    2.2.2 format fix

--- 2 Sorting/2.2/2.2.2/Program.cs
+++ 2 Sorting/2.2/2.2.2/Program.cs
@@ -17,20 +17,20 @@ namespace _2._2._2
     {
         static void Main(string[] args)
         {
-            // lo mi hi 0 1 2 3 4 5 6 7 8 9 10 11
-            //          E A S Y Q U E S T I O  N
-            // 0  0  1  A E S Y Q U E S T I O  N
-            // 0  1  2  A E S Y Q U E S T I O  N
-            // 3  3  4  A E S Q Y U E S T I O  N
-            // 3  4  5  A E S Q U Y E S T I O  N
-            // 0  2  5  A E S Q U Y E S T I O  N
-            // 6  6  7  A E S Q U Y E S T I O  N
-            // 6  7  8  A E S Q U Y E S T I O  N
-            // 9  9  10 A E S Q U Y E S T I O  N
-            // 9  10 11 A E S Q U Y E S T I N  O
-            // 6  8  11 A E S Q U Y E I N O S  T
-            // 0  5  11 A E E I N O Q S S T U  Y
-            //          A E E I N O Q S S T U  Y
+            // lo mi hi  0 1 2 3 4 5 6 7 8 9 10 11
+            //           E A S Y Q U E S T I O  N
+            // 0  0  1   A E S Y Q U E S T I O  N
+            // 0  1  2   A E S Y Q U E S T I O  N
+            // 3  3  4   A E S Q Y U E S T I O  N
+            // 3  4  5   A E S Q U Y E S T I O  N
+            // 0  2  5   A E S Q U Y E S T I O  N
+            // 6  6  7   A E S Q U Y E S T I O  N
+            // 6  7  8   A E S Q U Y E S T I O  N
+            // 9  9  10  A E S Q U Y E S T I O  N
+            // 9  10 11  A E S Q U Y E S T I N  O
+            // 6  8  11  A E S Q U Y E I N O S  T
+            // 0  5  11  A E E I N O Q S S T U  Y
+            //           A E E I N O Q S S T U  Y
         }
     }
 }

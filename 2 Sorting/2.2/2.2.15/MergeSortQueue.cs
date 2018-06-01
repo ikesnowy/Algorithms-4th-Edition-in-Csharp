3284f0ef6b6c427e6389548096620bafe4b26297
ikesnowy <ikesnowy@outlook.com>
2018-06-01 08:34:25 +0800

    2.2.15 Finished

---
+++ 2 Sorting/2.2/2.2.15/MergeSortQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._15
+{
+    class MergeSortQueue
+    {
+        /// <summary>
+        /// 默认构造函数。
+        /// </summary>
+        public MergeSortQueue() { }
+
+        /// <summary>
+        /// 利用队列归并进行自底向上的归并排序。
+        /// </summary>
+        /// <typeparam name="T">需要排序的元素类型。</typeparam>
+        /// <param name="a">需要排序的数组。</param>
+        public void Sort<T>(T[] a) where T : IComparable<T>
+        {
+            Queue<Queue<T>> queueList = new Queue<Queue<T>>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                Queue<T> temp = new Queue<T>();
+                temp.Enqueue(a[i]);
+                queueList.Enqueue(temp);
+            }
+
+            while (queueList.Size() != 1)
+            {
+                int times = queueList.Size() / 2;
+                for (int i = 0; i < times; i++)
+                {
+                    Queue<T> A = queueList.Dequeue();
+                    Queue<T> B = queueList.Dequeue();
+                    queueList.Enqueue(Merge(A, B));
+                }
+            }
+
+            Queue<T> result = queueList.Dequeue();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = result.Dequeue();
+            }
+        }
+
+        /// <summary>
+        /// 归并两个有序队列。输入队列将被清空。
+        /// </summary>
+        /// <typeparam name="T">有序队列的元素类型。</typeparam>
+        /// <param name="a">需要归并的队列。</param>
+        /// <param name="b">需要归并的队列。</param>
+        /// <returns>归并后的新队列。</returns>
+        public static Queue<T> Merge<T>(Queue<T> a, Queue<T> b) where T : IComparable<T>
+        {
+            Queue<T> sortedQueue = new Queue<T>();
+            while (!a.IsEmpty() && !b.IsEmpty())
+            {
+                if (a.Peek().CompareTo(b.Peek()) < 0)
+                    sortedQueue.Enqueue(a.Dequeue());
+                else
+                    sortedQueue.Enqueue(b.Dequeue());
+            }
+
+            while (!a.IsEmpty())
+                sortedQueue.Enqueue(a.Dequeue());
+            while (!b.IsEmpty())
+                sortedQueue.Enqueue(b.Dequeue());
+
+            return sortedQueue;
+        }
+    }
+}
---
+++ 2 Sorting/2.2/2.2.15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._15
+{
+    /*
+     * 2.2.15
+     * 
+     * 自底向上的有序队列归并排序。
+     * 用下面的方法编写一个自底向上的归并排序：
+     * 给定 N 个元素，创建 N 个队列，每个队列包含其中一个元素。
+     * 创建一个由这 N 个队列组成的队列，
+     * 然后不断用练习 2.2.14 中的方法将队列的头两个元素归并，
+     * 并将结果重新加入到队列结尾，
+     * 直到队列的队列只剩下一个元素为止。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
+            MergeSortQueue mergeSort = new MergeSortQueue();
+            mergeSort.Sort(a);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

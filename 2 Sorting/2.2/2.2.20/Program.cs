9e430dc1c56b75edcc7a9d6e2ccdea11afec29af
ikesnowy <ikesnowy@outlook.com>
2018-06-02 18:56:13 +0800

    2.2.20 Finished

---
+++ 2 Sorting/2.2/2.2.20/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._20
+{
+    /*
+     * 2.2.20
+     * 
+     * 间接排序。
+     * 编写一个不改变数组的归并排序，
+     * 它返回一个 int[] 数组 perm，
+     * 其中 perm[i] 的值是原数组中第 i 小的元素的位置。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MergeSort mergeSort = new MergeSort();
+            int[] a = new int[] { 2, 4, 6, 8, 1, 3, 5, 7, 9 };
+
+            int[] index = mergeSort.IndexSort(a);
+
+            Console.Write("Index \t");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Array\t");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Sorted\t");
+            for (int i = 0; i < index.Length; i++)
+            {
+                Console.Write(index[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

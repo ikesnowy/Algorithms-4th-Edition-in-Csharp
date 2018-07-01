4c2926f41c6ef3672214cd5509cf0744ca77b2d0
ikesnowy <ikesnowy@outlook.com>
2018-07-01 21:38:18 +0800

    删除不必要的using

--- 2 Sorting/2.2/2.2.2/Program.cs
+++ 2 Sorting/2.2/2.2.2/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2._2._2
+﻿namespace _2._2._2
 {
     /*
      * 2.2.2
--- 2 Sorting/2.2/2.2.4/Program.cs
+++ 2 Sorting/2.2/2.2.4/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2._2._4
+﻿namespace _2._2._4
 {
     /*
      * 2.2.4

464ef1d5528c7f4fb3b6d37d6ca40556705b06bd
ikesnowy <ikesnowy@outlook.com>
2018-08-05 21:49:59 +0800

    2.3.24 Finished

---
+++ 2 Sorting/2.3/2.3.24/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Quick;
+
+namespace _2._3._24
+{
+    /*
+     * 2.3.24
+     * 
+     * 取样排序。（W.Frazer，A.McKellar）
+     * 实现一个快速排序，
+     * 取样大小为 2^k-1。首先将取样得到的元素排序，
+     * 然后在递归函数中使用样品的中位数切分。
+     * 分为两部分的其余样品元素无需再次排序并可以分别应用于原数组的两个子数组。
+     * 这种算法称为取样排序。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickSort quickNormal = new QuickSort();
+            SampleSort sampleSort = new SampleSort();
+            int arraySize = 800000;                         // 初始数组大小。
+            const int trialTimes = 1;                       // 每次实验的重复次数。
+            const int trialLevel = 8;                       // 双倍递增的次数。
+
+            Console.WriteLine("n\t\tsample\tnormal\tratio");
+            for (int i = 0; i < trialLevel; i++)
+            {
+                double timeSample = 0;
+                double timeNormal = 0;
+                for (int j = 0; j < trialTimes; j++)
+                {
+                    int[] a = SortCompare.GetRandomArrayInt(arraySize);
+                    int[] b = new int[a.Length];
+                    a.CopyTo(b, 0);
+                    timeNormal += SortCompare.Time(quickNormal, b);
+                    timeSample += SortCompare.Time(sampleSort, a);
+
+                }
+                timeSample /= trialTimes;
+                timeNormal /= trialTimes;
+                if (arraySize < 10000000)
+                    Console.WriteLine(arraySize + "\t\t" + timeSample + "\t" + timeNormal + "\t" + timeSample / timeNormal);
+                else
+                    Console.WriteLine(arraySize + "\t" + timeSample + "\t" + timeNormal + "\t" + timeSample / timeNormal);
+                arraySize *= 2;
+            }
+        }
+    }
+}
---
+++ 2 Sorting/2.3/Quick/SampleSort.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Quick
+{
+    /// <summary>
+    /// 取样排序类。
+    /// </summary>
+    public class SampleSort : QuickSort
+    {
+        /// <summary>
+        /// 取样数组长度 2^k - 1 的阶数。
+        /// </summary>
+        public int K { get; set; }
+
+        /// <summary>
+        /// 默认构造函数。
+        /// </summary>
+        public SampleSort()
+        {
+            this.K = 8;
+        }
+
+        /// <summary>
+        /// 用快速排序对数组 a 进行升序排序。
+        /// </summary>
+        /// <typeparam name="T">需要排序的类型。</typeparam>
+        /// <param name="a">需要排序的数组。</param>
+        public override void Sort<T>(T[] a)
+        {
+            if (a.Length < Math.Pow(2, this.K + 1))
+            {
+                // 小于 2^(k+1) 的数组直接进行快排
+                base.Sort(a);
+                return;
+            }
+
+            Shuffle(a);
+            int samplehi = (int)Math.Pow(2, this.K) - 1;
+            // 利用快速排序对取样数组进行排序
+            base.Sort(a, 0, samplehi);
+            // 找到取样数组的中位数
+            int sampleMedian = samplehi / 2;
+            // 将取样数组后半部分放到数组末尾
+            int i = samplehi, j = a.Length - 1;
+            while (i != sampleMedian)
+                Exch(a, i--, j--);
+            // 根据取样数组进行排序
+            Sort(a, 0, sampleMedian, j, a.Length - 1);
+            Debug.Assert(IsSorted(a));
+        }
+
+        /// <summary>
+        /// 用快速排序对数组 a 的 lo ~ hi 范围排序。
+        /// </summary>
+        /// <typeparam name="T">需要排序的数组类型。</typeparam>
+        /// <param name="a">需要排序的数组。</param>
+        /// <param name="samplelo">取样数组的起始下标。</param>
+        /// <param name="lo">排序范围的起始下标。</param>
+        /// <param name="hi">排序范围的结束下标。</param>
+        /// <param name="samplehi">取样数组的终止下标。</param>
+        private void Sort<T>(T[] a, int samplelo, int lo, int hi, int samplehi) where T : IComparable<T>
+        {
+            if (hi <= lo)                   // 别越界
+                return;
+
+            int j = Partition(a, lo, hi);
+            // 将前部的有序取样数组取半，后半部分放在枢轴前面。
+            if (lo - samplelo > 1)
+            {
+                // p 应该始终指向有序部分的最后一项
+                // v 应该始终指向有序部分的前面一项
+                int p = lo - 1, v = j - 1;
+                for (int i = 0; i < (lo - samplelo) / 2; i++)
+                {
+                    Exch(a, p--, v--);
+                }
+                Sort(a, samplelo, p, v, j - 1);
+            }
+            else
+            {
+                // 取样数组已经用完，退化为普通 Quicksort
+                base.Sort(a, samplelo, j - 1);
+            }
+
+            // 将尾部有序取样数组取半，前半部分放在枢轴后面。
+            if (samplehi - hi > 1)
+            {
+                // p 应该始终指向有序部分的前面一项
+                // v 应该始终指向有序部分的最后一项
+                int p = hi, v = j;
+                for (int i = 0; i < (samplehi - hi) / 2; i++)
+                {
+                    Exch(a, ++p, ++v);
+                }
+                Sort(a, j + 1, v, p, samplehi);
+            }
+            else
+            {
+                // 取样数组已用完，退化为普通 Quicksort
+                base.Sort(a, j + 1, samplehi);
+            }
+        }
+
+        /// <summary>
+        /// 对数组进行切分，返回枢轴位置。
+        /// </summary>
+        /// <typeparam name="T">需要切分的数组类型。</typeparam>
+        /// <param name="a">需要切分的数组。</param>
+        /// <param name="lo">切分的起始点。</param>
+        /// <param name="hi">切分的末尾点。</param>
+        /// <returns>枢轴下标。</returns>
+        private int Partition<T>(T[] a, int lo, int hi) where T : IComparable<T>
+        {
+            int i = lo, j = hi + 1;
+            T v = a[lo];
+            while (true)
+            {
+                while (Less(a[++i], v))
+                    if (i == hi)
+                        break;
+                while (Less(v, a[--j]))
+                    if (j == lo)
+                        break;
+                if (i >= j)
+                    break;
+                Exch(a, i, j);
+            }
+            Exch(a, lo, j);
+            return j;
+        }
+
+        /// <summary>
+        /// 打乱数组。
+        /// </summary>
+        /// <typeparam name="T">需要打乱的数组类型。</typeparam>
+        /// <param name="a">需要打乱的数组。</param>
+        private void Shuffle<T>(T[] a)
+        {
+            Random random = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                int r = i + random.Next(a.Length - i);
+                T temp = a[i];
+                a[i] = a[r];
+                a[r] = temp;
+            }
+        }
+    }
+}

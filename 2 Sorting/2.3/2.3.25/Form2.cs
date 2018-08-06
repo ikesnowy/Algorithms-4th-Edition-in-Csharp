db080e953f8bf638bd7ae2c6dc7764f050bcfaac
ikesnowy <ikesnowy@outlook.com>
2018-08-06 19:09:37 +0800

    2.3.25 Finished

---
+++ 2 Sorting/2.3/2.3.25/Form2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Quick;
+
+namespace _2._3._25
+{
+    public partial class Form2 : Form
+    {
+        /// <summary>
+        /// 测试数组大小。
+        /// </summary>
+        public int N = 100;
+
+        public Form2(int n)
+        {
+            InitializeComponent();
+            this.N = n;
+        }
+
+        private void Form2_Shown(object sender, EventArgs e)
+        {
+            this.Text = "正在绘图";
+            this.backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            QuickSortInsertion quickSortInsertion = new QuickSortInsertion();
+            double[] timeRecord = new double[31];
+            for (int i = 0; i <= 30; i++)
+            {
+                worker.ReportProgress(i * 3);
+                quickSortInsertion.M = i;
+                int[] data = SortCompare.GetRandomArrayInt(this.N);
+                timeRecord[i] = SortCompare.Time(quickSortInsertion, data);
+            }
+            e.Result = timeRecord;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.Text = "正在绘图，已完成 " + e.ProgressPercentage + " %";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            double[] result = e.Result as double[];
+
+            Graphics graphics = this.CreateGraphics();
+
+            // 获得绘图区矩形。
+            RectangleF rect = this.ClientRectangle;
+            float unitX = rect.Width / 10;
+            float unitY = rect.Width / 10;
+
+            // 添加 10% 边距作为文字区域。
+            RectangleF center = new RectangleF
+                (rect.X + unitX, rect.Y + unitY,
+                rect.Width - 2 * unitX, rect.Height - 2 * unitY);
+
+            // 绘制坐标系。
+            graphics.DrawLine(Pens.Black, center.Left, center.Top, center.Left, center.Bottom);
+            graphics.DrawLine(Pens.Black, center.Left, center.Bottom, center.Right, center.Bottom);
+            graphics.DrawString(result.Max().ToString(), this.Font, Brushes.Black, rect.Location);
+            graphics.DrawString(result.Length.ToString(), this.Font, Brushes.Black, center.Right, center.Bottom);
+            graphics.DrawString("0", this.Font, Brushes.Black, rect.Left, center.Bottom);
+
+            // 初始化点。
+            PointF[] bluePoints = new PointF[result.Length];
+            unitX = center.Width / result.Length;
+            unitY = center.Height / (float)result.Max();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                bluePoints[i] = new PointF(center.Left + unitX * (i + 1), center.Bottom - (float)(result[i] * unitY) - 10);
+            }
+
+            // 绘制点。
+            for (int i = 0; i < result.Length; i++)
+            {
+                graphics.FillEllipse(Brushes.Blue, new RectangleF(bluePoints[i], new Size(10, 10)));
+            }
+
+            graphics.Dispose();
+
+            this.Text = "绘图结果";
+            int min = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] < result[min])
+                    min = i;
+            }
+            string report = "M " + min + "\r\ntime " + result[min];
+            MessageBox.Show(report, "最优结果");
+        }
+    }
+}
---
+++ 2 Sorting/2.3/2.3.25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _2._3._25
+{
+    /*
+     * 2.3.25
+     * 
+     * 切换到插入排序。
+     * 实现一个快速排序，在子数组元素少于 M 时切换到插入排序。
+     * 用快速排序处理大小 N 分别为 10^3、10^4、10^5 和 10^6 的随机数组，
+     * 根据经验给出使其在你的环境中运行速度最快的 M 值。
+     * 将 M 从 0 变化到 30 的每个值所得到的平均运行时间绘成曲线。
+     * 注意：
+     * 你需要为算法 2.2 添加一个需要三个参数的 sort() 方法
+     * 以使 Insertion.sort(a, lo, hi) 将子数组 a[lo...hi] 排序。
+     * 
+     */
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}

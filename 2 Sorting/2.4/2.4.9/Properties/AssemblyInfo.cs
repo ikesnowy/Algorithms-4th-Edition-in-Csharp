184ef52b0512529330e5fc921c2e4218de5376e5
ikesnowy <ikesnowy@outlook.com>
2018-08-17 22:13:49 +0800

    2.4.9 Finished

--- 2 Sorting/2.4/2.4.9/Properties/AssemblyInfo.cs
+++ 2 Sorting/2.4/2.4.9/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("2.4.8")]
+[assembly: AssemblyTitle("2.4.9")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("2.4.8")]
+[assembly: AssemblyProduct("2.4.9")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("cfd13dbb-36d9-43e1-8e78-a29d2236be79")]
+[assembly: Guid("ecc51890-e3b6-4d06-a7a7-92128a211617")]
 
 // 程序集的版本信息由下列四个值组成: 
 //

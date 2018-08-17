8aed2aad284491cd419b74c17d866dfde4f9e84e
ikesnowy <ikesnowy@outlook.com>
2018-08-17 16:41:37 +0800

    2.4.8 Finished

---
+++ 2 Sorting/2.4/2.4.8/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _2._4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 结合习题 2.4.7 的结果不难推出：
+            // 第 k 大元素只可能出现在深度＜k 的位置（k>=2）
+            // 即 2^k - 1，(k>=2)
+        }
+    }
+}

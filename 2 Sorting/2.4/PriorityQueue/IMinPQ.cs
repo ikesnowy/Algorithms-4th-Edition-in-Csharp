9658e0254f3ac0ab4712802af597e58f19008ca1
ikesnowy <ikesnowy@outlook.com>
2018-08-21 10:57:42 +0800

    Implement MinPQ

--- 2 Sorting/2.4/PriorityQueue/IMinPQ.cs
+++ 2 Sorting/2.4/PriorityQueue/IMinPQ.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PriorityQueue
 {
     /// <summary>
-    /// 实现优先队列 API 的接口。（最大堆）
+    /// 实现优先队列 API 的接口。（最小堆）
     /// </summary>
     /// <typeparam name="Key">优先队列容纳的元素。</typeparam>
-    public interface IMaxPQ<Key> where Key : IComparable<Key>
+    public interface IMinPQ<Key> where Key : IComparable<Key>
     {
         /// <summary>
         /// 向优先队列中插入一个元素。
@@ -15,16 +19,16 @@ namespace PriorityQueue
         void Insert(Key v);
 
         /// <summary>
-        /// 返回最大元素。
+        /// 返回最小元素。
         /// </summary>
         /// <returns></returns>
-        Key Max();
+        Key Min();
 
         /// <summary>
-        /// 删除并返回最大元素。
+        /// 删除并返回最小元素。
         /// </summary>
         /// <returns></returns>
-        Key DelMax();
+        Key DelMin();
 
         /// <summary>
         /// 返回队列是否为空。
--- 2 Sorting/2.4/PriorityQueue/MinPQ.cs
+++ 2 Sorting/2.4/PriorityQueue/MinPQ.cs
@@ -6,10 +6,10 @@ using System.Diagnostics;
 namespace PriorityQueue
 {
     /// <summary>
-    /// 最大堆。（数组实现）
+    /// 最小堆。（数组实现）
     /// </summary>
-    /// <typeparam name="Key">最大堆中保存的元素类型。</typeparam>
-    public class MaxPQ<Key> : IMaxPQ<Key>, IEnumerable<Key> where Key : IComparable<Key>
+    /// <typeparam name="Key">最小堆中保存的元素类型。</typeparam>
+    public class MinPQ<Key> : IMinPQ<Key>, IEnumerable<Key> where Key : IComparable<Key>
     {
         private Key[] pq;               // 保存元素的数组。
         private int n;                  // 堆中的元素数量。
@@ -17,23 +17,23 @@ namespace PriorityQueue
         /// <summary>
         /// 默认构造函数。
         /// </summary>
-        public MaxPQ() : this(1) { }
+        public MinPQ() : this(1) { }
 
         /// <summary>
-        /// 建立指定容量的最大堆。
+        /// 建立指定容量的最小堆。
         /// </summary>
-        /// <param name="capacity">最大堆的容量。</param>
-        public MaxPQ(int capacity)
+        /// <param name="capacity">最小堆的容量。</param>
+        public MinPQ(int capacity)
         {
             this.pq = new Key[capacity + 1];
             this.n = 0;
         }
 
         /// <summary>
-        /// 从已有元素建立一个最大堆。（O(n)）
+        /// 从已有元素建立一个最小堆。（O(n)）
         /// </summary>
         /// <param name="keys">已有元素。</param>
-        public MaxPQ(Key[] keys)
+        public MinPQ(Key[] keys)
         {
             this.n = keys.Length;
             this.pq = new Key[keys.Length + 1];
@@ -41,19 +41,19 @@ namespace PriorityQueue
                 this.pq[i + 1] = keys[i];
             for (int k = this.n / 2; k >= 1; k--)
                 Sink(k);
-            Debug.Assert(IsMaxHeap());
+            Debug.Assert(IsMinHeap());
         }
 
         /// <summary>
-        /// 删除并返回最大元素。
+        /// 删除并返回最小元素。
         /// </summary>
         /// <returns></returns>
-        public Key DelMax()
+        public Key DelMin()
         {
             if (IsEmpty())
                 throw new ArgumentOutOfRangeException("Priority Queue Underflow");
 
-            Key max = this.pq[1];
+            Key min = this.pq[1];
             Exch(1, this.n--);
             Sink(1);
             this.pq[this.n + 1] = default(Key);
@@ -61,7 +61,7 @@ namespace PriorityQueue
                 Resize(this.pq.Length / 2);
 
             // Debug.Assert(IsMaxHeap());
-            return max;
+            return min;
         }
 
         /// <summary>
@@ -85,10 +85,10 @@ namespace PriorityQueue
         public bool IsEmpty() => this.n == 0;
 
         /// <summary>
-        /// 获得堆中最大元素。
+        /// 获得堆中最小元素。
         /// </summary>
         /// <returns></returns>
-        public Key Max() => this.pq[1];
+        public Key Min() => this.pq[1];
 
         /// <summary>
         /// 获得堆中元素的数量。
@@ -125,7 +125,7 @@ namespace PriorityQueue
         /// <param name="k">需要上浮的元素。</param>
         private void Swim(int k)
         {
-            while (k > 1 && Less(k / 2, k))
+            while (k > 1 && Greater(k / 2, k))
             {
                 Exch(k, k / 2);
                 k /= 2;
@@ -141,9 +141,9 @@ namespace PriorityQueue
             while (k * 2 <= this.n)
             {
                 int j = 2 * k;
-                if (Less(j, j + 1))
+                if (Greater(j, j + 1))
                     j++;
-                if (!Less(k, j))
+                if (!Greater(k, j))
                     break;
                 Exch(k, j);
                 k = j;
@@ -165,13 +165,13 @@ namespace PriorityQueue
         }
 
         /// <summary>
-        /// 判断堆中某个元素是否小于另一元素。
+        /// 判断堆中某个元素是否大于另一元素。
         /// </summary>
-        /// <param name="i">判断是否较小的元素。</param>
-        /// <param name="j">判断是否较大的元素。</param>
+        /// <param name="i">判断是否较大的元素。</param>
+        /// <param name="j">判断是否较小的元素。</param>
         /// <returns></returns>
-        private bool Less(int i, int j)
-            => this.pq[i].CompareTo(this.pq[j]) < 0; 
+        private bool Greater(int i, int j)
+            => this.pq[i].CompareTo(this.pq[j]) > 0;
 
         /// <summary>
         /// 交换堆中的两个元素。
@@ -186,28 +186,28 @@ namespace PriorityQueue
         }
 
         /// <summary>
-        /// 检查当前二叉树是不是一个最大堆。
+        /// 检查当前二叉树是不是一个最小堆。
         /// </summary>
         /// <returns></returns>
-        private bool IsMaxHeap() => IsMaxHeap(1);
+        private bool IsMinHeap() => IsMinHeap(1);
 
         /// <summary>
-        /// 确定以 k 为根节点的二叉树是不是一个最大堆。
+        /// 确定以 k 为根节点的二叉树是不是一个最小堆。
         /// </summary>
         /// <param name="k">需要检查的二叉树根节点。</param>
         /// <returns></returns>
-        private bool IsMaxHeap(int k)
+        private bool IsMinHeap(int k)
         {
             if (k > this.n)
                 return true;
             int left = 2 * k;
             int right = 2 * k + 1;
-            if (left <= this.n && Less(k, left))
+            if (left <= this.n && Greater(k, left))
                 return false;
-            if (right <= this.n && Less(k, right))
+            if (right <= this.n && Greater(k, right))
                 return false;
 
-            return IsMaxHeap(left) && IsMaxHeap(right);
+            return IsMinHeap(left) && IsMinHeap(right);
         }
     }
 }

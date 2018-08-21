0bbd04a3c0cf130eff16938d9c5942631ab90725
ikesnowy <ikesnowy@outlook.com>
2018-08-21 21:22:54 +0800

    Implement Heap.Sort

---
+++ 2 Sorting/2.4/2.4.16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._4._16
+{
+    /*
+     * 2.4.16
+     * 
+     * 对于 N=32，构造数组使得堆排序使用的比较次数最多以及最少。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 11, 8, 7, 6, 5, 4, 3, 2, 1 };
+            PriorityQueue.Heap.Sort(a);
+            foreach (int i in a)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+    }
+}

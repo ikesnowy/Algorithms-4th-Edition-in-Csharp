3ffe28610c2aedecce6f4fb396dd194b486b0307
ikesnowy <ikesnowy@outlook.com>
2018-09-07 22:30:04 +0800

    2.4.16 Finished

--- 2 Sorting/2.4/2.4.16/Program.cs
+++ 2 Sorting/2.4/2.4.16/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PriorityQueue;
 
 namespace _2._4._16
 {
@@ -19,13 +20,31 @@ namespace _2._4._16
             // 最好情况
             // 比较简单，键值完全相同的数组即可。
             // 最坏情况
-            // 
+            // 参考论文：https://arxiv.org/abs/1504.01459
+            // 下面的代码能够生成最好和最坏情况并输出序列和比较次数。
+            int size = 31;
+            int[] bestCase = new int[size];
+            for (int i = 0; i < size; i++)
+                bestCase[i] = 1;
 
-            int[] a = { 11, 8, 7, 6, 5, 4, 3, 2, 1 };
-            PriorityQueue.Heap.Sort(a);
-            foreach (int i in a)
-                Console.Write(i + " ");
+            Console.WriteLine("Best Input");
+            for (int i = 0; i < size; i++)
+                Console.Write(bestCase[i] + " ");
             Console.WriteLine();
+            Console.Write("Best Compare Times: ");
+            Console.WriteLine(HeapAnalysis.Sort(bestCase));
+
+            Console.WriteLine();
+
+
+            MaxPQWorstCase pq = new MaxPQWorstCase(size);
+            int[] worstCase = pq.MakeWorst(size);
+            Console.WriteLine("Worst Input");
+            for (int i = 0; i < worstCase.Length; i++)
+                Console.Write(worstCase[i] + " ");
+            Console.WriteLine();
+            Console.Write("Worst Compare Times: ");
+            Console.WriteLine(HeapAnalysis.Sort(worstCase));
         }
     }
 }

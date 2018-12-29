34f118722e3cd41dad0ecf0f312ca94ee7a8386a
沈星繁 <ikesnowy@outlook.com>
2018-12-29 18:36:06 +0800

    2.4.40 Finished

--- 2 Sorting/2.4/PriorityQueue/HeapFloyd.cs
+++ 2 Sorting/2.4/PriorityQueue/HeapFloyd.cs
@@ -3,22 +3,17 @@
 namespace PriorityQueue
 {
     /// <summary>
-    /// 记录比较次数的堆排序。
+    /// 堆排序类，提供 Floyd 优化的堆排序的静态方法。
     /// </summary>
-    public static class HeapAnalysis
+    /// <typeparam name="T">需要排序的元素类型。</typeparam>
+    public static class HeapFloyd
     {
         /// <summary>
-        /// 堆排序的比较次数。
-        /// </summary>
-        private static long compareTimes;
-
-        /// <summary>
-        /// 利用堆排序对数组进行排序，返回比较次数。
+        /// 利用堆排序对数组进行排序。
         /// </summary>
         /// <param name="pq">需要排序的数组。</param>
-        public static long Sort<T>(T[] pq) where T : IComparable<T>
+        public static void Sort<T>(T[] pq) where T : IComparable<T>
         {
-            compareTimes = 0;
             int n = pq.Length;
             // 建堆
             for (int k = n / 2; k >= 1; k--)
@@ -29,9 +24,8 @@ namespace PriorityQueue
             while (n > 1)
             {
                 Exch(pq, 1, n--);
-                Sink(pq, 1, n);
+                SinkThenSwim(pq, 1, n);
             }
-            return compareTimes;
         }
 
         /// <summary>
@@ -55,17 +49,46 @@ namespace PriorityQueue
         }
 
         /// <summary>
+        /// 先下沉后上浮。
+        /// </summary>
+        /// <typeparam name="T">堆中的元素类型。</typeparam>
+        /// <param name="pq">包含堆元素的数组。</param>
+        /// <param name="k">要下沉的元素。</param>
+        /// <param name="n">元素数量。</param>
+        private static void SinkThenSwim<T>(T[] pq, int k, int n) where T : IComparable<T>
+        {
+            while (2 * k <= n)
+            {
+                int j = 2 * k;
+                if (j < n && Less(pq, j, j + 1))
+                    j++;
+                Exch(pq, k, j);
+                k = j;
+            }
+            Swim(pq, k);
+        }
+
+        /// <summary>
+        /// 使元素上浮。
+        /// </summary>
+        /// <param name="k">需要上浮的元素。</param>
+        private static void Swim<T>(T[] pq, int k) where T : IComparable<T>
+        {
+            while (k > 1 && Less(pq, k / 2, k))
+            {
+                Exch(pq, k, k / 2);
+                k /= 2;
+            }
+        }
+
+        /// <summary>
         /// 比较堆中下标为 <paramref name="a"/> 的元素是否小于下标为 <paramref name="b"/> 的元素。
         /// </summary>
         /// <param name="pq">元素所在的数组。</param>
         /// <param name="a">需要比较是否较小的结点序号。</param>
         /// <param name="b">需要比较是否较大的结点序号。</param>
         /// <returns></returns>
-        private static bool Less<T>(T[] pq, int a, int b) where T : IComparable<T>
-        {
-            compareTimes++;
-            return pq[a - 1].CompareTo(pq[b - 1]) < 0;
-        }
+        private static bool Less<T>(T[] pq, int a, int b) where T : IComparable<T> => pq[a - 1].CompareTo(pq[b - 1]) < 0;
 
         /// <summary>
         /// 交换堆中的两个元素。
--- 2 Sorting/2.4/PriorityQueue/HeapFloydAnalysis.cs
+++ 2 Sorting/2.4/PriorityQueue/HeapFloydAnalysis.cs
@@ -3,22 +3,20 @@
 namespace PriorityQueue
 {
     /// <summary>
-    /// 记录比较次数的堆排序。
+    /// 堆排序类，提供 Floyd 优化的堆排序的静态方法。
     /// </summary>
-    public static class HeapAnalysis
+    /// <typeparam name="T">需要排序的元素类型。</typeparam>
+    public static class HeapFloydAnalysis
     {
-        /// <summary>
-        /// 堆排序的比较次数。
-        /// </summary>
-        private static long compareTimes;
+        private static long compareCount;
 
         /// <summary>
-        /// 利用堆排序对数组进行排序，返回比较次数。
+        /// 利用堆排序对数组进行排序。返回比较次数。
         /// </summary>
         /// <param name="pq">需要排序的数组。</param>
         public static long Sort<T>(T[] pq) where T : IComparable<T>
         {
-            compareTimes = 0;
+            compareCount = 0;
             int n = pq.Length;
             // 建堆
             for (int k = n / 2; k >= 1; k--)
@@ -29,9 +27,9 @@ namespace PriorityQueue
             while (n > 1)
             {
                 Exch(pq, 1, n--);
-                Sink(pq, 1, n);
+                SinkThenSwim(pq, 1, n);
             }
-            return compareTimes;
+            return compareCount;
         }
 
         /// <summary>
@@ -55,6 +53,39 @@ namespace PriorityQueue
         }
 
         /// <summary>
+        /// 先下沉后上浮。
+        /// </summary>
+        /// <typeparam name="T">堆中的元素类型。</typeparam>
+        /// <param name="pq">包含堆元素的数组。</param>
+        /// <param name="k">要下沉的元素。</param>
+        /// <param name="n">元素数量。</param>
+        private static void SinkThenSwim<T>(T[] pq, int k, int n) where T : IComparable<T>
+        {
+            while (2 * k <= n)
+            {
+                int j = 2 * k;
+                if (j < n && Less(pq, j, j + 1))
+                    j++;
+                Exch(pq, k, j);
+                k = j;
+            }
+            Swim(pq, k);
+        }
+
+        /// <summary>
+        /// 使元素上浮。
+        /// </summary>
+        /// <param name="k">需要上浮的元素。</param>
+        private static void Swim<T>(T[] pq, int k) where T : IComparable<T>
+        {
+            while (k > 1 && Less(pq, k / 2, k))
+            {
+                Exch(pq, k, k / 2);
+                k /= 2;
+            }
+        }
+
+        /// <summary>
         /// 比较堆中下标为 <paramref name="a"/> 的元素是否小于下标为 <paramref name="b"/> 的元素。
         /// </summary>
         /// <param name="pq">元素所在的数组。</param>
@@ -63,7 +94,7 @@ namespace PriorityQueue
         /// <returns></returns>
         private static bool Less<T>(T[] pq, int a, int b) where T : IComparable<T>
         {
-            compareTimes++;
+            compareCount++;
             return pq[a - 1].CompareTo(pq[b - 1]) < 0;
         }
 

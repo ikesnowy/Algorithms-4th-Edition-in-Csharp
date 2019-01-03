af7cbf68d4a41343876fa66118b0475b82d2a3bb
沈星繁 <ikesnowy@outlook.com>
2019-01-03 15:23:06 +0800

    2.5.1 Finished

---
+++ 2 Sorting/2.5/2.5.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._5._1
+{
+    /*
+     * 2.5.1
+     * 
+     * 在下面这段 String 类型的 compareTo() 方法的实现中，
+     * 第三行对提高运行效率有何帮助？
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if (this == that) return 0;
+            // 如果比较的两个 string 是同一个对象的引用，直接返回相等结果
+            // 而不必再逐字符比较。
+            string s = "123456";
+            string p = s;
+            Console.WriteLine(s.CompareTo(p));  // 输出 0
+        }
+    }
+}
--- 2 Sorting/2.5/2.5.1/Properties/AssemblyInfo.cs
+++ 2 Sorting/2.5/2.5.1/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SortApplication")]
+[assembly: AssemblyTitle("2.5.1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SortApplication")]
+[assembly: AssemblyProduct("2.5.1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("121d6b05-a47f-4c1c-bad8-ee4e5a245c8e")]
+[assembly: Guid("cc3f5e6e-ee3b-41da-8098-50d38cd55a7f")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      修订号
 //
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-//通过使用 "*"，如下所示:
+// 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

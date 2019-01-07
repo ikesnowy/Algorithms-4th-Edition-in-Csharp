29dfcf4c646dd190bed7f863b3809a354774913f
沈星繁 <ikesnowy@outlook.com>
2019-01-07 14:49:55 +0800

    2.5.7 Finished

---
+++ 2 Sorting/2.5/2.5.7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using SortApplication;
+
+namespace _2._5._7
+{
+    /*
+     * 2.5.7
+     * 
+     * 用 select() 找出 N 个元素中的最小值平均大约需要多少次比较？
+     * 
+     */
+    class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            // 参考快速排序的分析
+            // Cn 代表找出 n 个元素的最小值所需的比较次数
+            // Cn = 1/n * (n+1) + 1/n * (n+1+C0) + 1/n * (n+1+C1) +...+ 1/n * (n+1+C(n-1))
+            // Cn = n+1 + 1/n * (C0 + C1 + ... + C(n-1))
+            // nCn = n(n+1) + C0 + ... + C(n-1)
+            // (n-1)C(n-1) = n(n-1) + C0 + ... + C(n-2)
+            // nCn - (n-1)C(n-1) = 2n + C(n-1)
+            // nCn = 2n + nC(n-1)
+            // Cn = 2 + C(n-1)
+            // Cn = 2n-3, n > 1.
+
+            int multiBy10 = 5;
+            int repeatTime = 100;
+            int n = 10000;
+            Console.WriteLine("n\tAverage Compare");
+            for (int i = 0; i < multiBy10; i++)
+            {
+                long totalCompare = 0;
+                for (int j = 0; j < repeatTime; j++)
+                {
+                    QuickSortAnalyze quick = new QuickSortAnalyze();
+                    quick.Select(GetRandomArray(n), 0);
+                    totalCompare += quick.CompareCount;
+                }
+                Console.WriteLine("10^" + (i + 4) + "\t" + totalCompare / repeatTime);
+                n *= 10;
+            }
+
+        }
+
+        static int[] GetRandomArray(int n)
+        {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next();
+            }
+            return array;
+        }
+    }
+}
--- 2 Sorting/2.5/2.5.7/Properties/AssemblyInfo.cs
+++ 2 Sorting/2.5/2.5.7/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("2.5.6")]
+[assembly: AssemblyTitle("2.5.7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("2.5.6")]
+[assembly: AssemblyProduct("2.5.7")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("c34a21b4-33a4-434d-a17f-be0e92803cca")]
+[assembly: Guid("f85a0396-6b6c-4694-8d65-b25fa8ba951e")]
 
 // 程序集的版本信息由下列四个值组成: 
 //

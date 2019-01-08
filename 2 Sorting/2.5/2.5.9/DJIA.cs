6dd596bc8d4599afbfdc13c30b9f9ebe242540c3
沈星繁 <ikesnowy@outlook.com>
2019-01-08 14:41:23 +0800

    2.5.9 Finished

---
+++ 2 Sorting/2.5/2.5.9/DJIA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5._9
+{
+    /// <summary>
+    /// 道琼斯指数。
+    /// </summary>
+    class DJIA : IComparable<DJIA>
+    {
+        public string Date { get; set; }
+        public long Volume { get; set; }
+
+        public DJIA(string date, long vol)
+        {
+            this.Date = date;
+            this.Volume = vol;
+        }
+
+        public int CompareTo(DJIA other)
+        {
+            return this.Volume.CompareTo(other.Volume);
+        }
+    }
+}

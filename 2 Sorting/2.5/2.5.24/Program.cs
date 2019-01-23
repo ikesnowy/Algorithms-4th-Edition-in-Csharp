2fefcde621dcf29e0fd83ba2f30a160aaf91182c
沈星繁 <ikesnowy@outlook.com>
2019-01-23 20:05:28 +0800

    2.5.24 Finished

---
+++ 2 Sorting/2.5/2.5.24/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SortApplication;
+
+namespace _2._5._24
+{
+    /*
+     * 2.5.24
+     * 
+     * 稳定的优先队列。
+     * 实现一个稳定的优先队列（将重复的元素按照它们被插入的顺序返回）。
+     * 
+     */
+    class Program
+    {
+        class Wrapper<T> : IComparable<Wrapper<T>> where T : IComparable<T> 
+        {
+            public T Value;
+            public int Index;
+
+            public Wrapper(T value, int index)
+            {
+                this.Value = value;
+                this.Index = index;
+            }
+
+            public int CompareTo(Wrapper<T> other)
+            {
+                return this.Value.CompareTo(other.Value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MinPQStable<Wrapper<string>> pq = new MinPQStable<Wrapper<string>>();
+            string text = "it was the best of times it was the worst of times it was the ";
+            string[] texts = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < texts.Length; i++)
+                pq.Insert(new Wrapper<string>(texts[i], i + 1));
+
+            while (!pq.IsEmpty())
+            {
+                Wrapper<string> w = pq.DelMin();
+                Console.WriteLine(w.Value + " " + w.Index);
+            }
+        }
+    }
+}
---
+++ 2 Sorting/2.5/SortApplication/MinPQStable.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SortApplication
+{
+    /// <summary>
+    /// 稳定的最小堆。（数组实现）
+    /// </summary>
+    /// <typeparam name="Key">最小堆中保存的元素类型。</typeparam>
+    public class MinPQStable<Key> : IMinPQ<Key>, IEnumerable<Key> where Key : IComparable<Key>
+    {
+        protected Key[] pq;               // 保存元素的数组。
+        protected int n;                  // 堆中的元素数量。
+        private long[] time;              // 元素的插入次序。
+        private long timeStamp = 1;       // 元素插入次序计数器。
+
+        /// <summary>
+        /// 默认构造函数。
+        /// </summary>
+        public MinPQStable() : this(1) { }
+
+        /// <summary>
+        /// 建立指定容量的最小堆。
+        /// </summary>
+        /// <param name="capacity">最小堆的容量。</param>
+        public MinPQStable(int capacity)
+        {
+            this.time = new long[capacity + 1];
+            this.pq = new Key[capacity + 1];
+            this.n = 0;
+        }
+
+        /// <summary>
+        /// 删除并返回最小元素。
+        /// </summary>
+        /// <returns></returns>
+        public Key DelMin()
+        {
+            if (IsEmpty())
+                throw new ArgumentOutOfRangeException("Priority Queue Underflow");
+
+            Key min = this.pq[1];
+            Exch(1, this.n--);
+            Sink(1);
+            this.pq[this.n + 1] = default(Key);
+            this.time[this.n + 1] = 0;
+            if ((this.n > 0) && (this.n == this.pq.Length / 4))
+                Resize(this.pq.Length / 2);
+
+            //Debug.Assert(IsMinHeap());
+            return min;
+        }
+
+        /// <summary>
+        /// 向堆中插入一个元素。
+        /// </summary>
+        /// <param name="v">需要插入的元素。</param>
+        public void Insert(Key v)
+        {
+            if (this.n == this.pq.Length - 1)
+                Resize(2 * this.pq.Length);
+
+            this.pq[++this.n] = v;
+            this.time[this.n] = ++this.timeStamp;
+            Swim(this.n);
+            //Debug.Assert(IsMinHeap());
+        }
+
+        /// <summary>
+        /// 检查堆是否为空。
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty() => this.n == 0;
+
+        /// <summary>
+        /// 获得堆中最小元素。
+        /// </summary>
+        /// <returns></returns>
+        public Key Min() => this.pq[1];
+
+        /// <summary>
+        /// 获得堆中元素的数量。
+        /// </summary>
+        /// <returns></returns>
+        public int Size() => this.n;
+
+        /// <summary>
+        /// 获取堆的迭代器，元素以降序排列。
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<Key> GetEnumerator()
+        {
+            MaxPQ<Key> copy = new MaxPQ<Key>(this.n);
+            for (int i = 1; i <= this.n; i++)
+                copy.Insert(this.pq[i]);
+
+            while (!copy.IsEmpty())
+                yield return copy.DelMax(); // 下次迭代的时候从这里继续执行。
+        }
+
+        /// <summary>
+        /// 获取堆的迭代器，元素以降序排列。
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// 使元素上浮。
+        /// </summary>
+        /// <param name="k">需要上浮的元素。</param>
+        private void Swim(int k)
+        {
+            while (k > 1 && Greater(k / 2, k))
+            {
+                Exch(k, k / 2);
+                k /= 2;
+            }
+        }
+
+        /// <summary>
+        /// 使元素下沉。
+        /// </summary>
+        /// <param name="k">需要下沉的元素。</param>
+        private void Sink(int k)
+        {
+            while (k * 2 <= this.n)
+            {
+                int j = 2 * k;
+                if (j < this.n && Greater(j, j + 1))
+                    j++;
+                if (!Greater(k, j))
+                    break;
+                Exch(k, j);
+                k = j;
+            }
+        }
+
+        /// <summary>
+        /// 重新调整堆的大小。
+        /// </summary>
+        /// <param name="capacity">调整后的堆大小。</param>
+        private void Resize(int capacity)
+        {
+            Key[] temp = new Key[capacity];
+            long[] timeTemp = new long[capacity];
+            for (int i = 1; i <= this.n; i++)
+            {
+                temp[i] = this.pq[i];
+                timeTemp[i] = this.time[i];
+            }
+            this.pq = temp;
+            this.time = timeTemp;
+        }
+
+        /// <summary>
+        /// 判断堆中某个元素是否大于另一元素。
+        /// </summary>
+        /// <param name="i">判断是否较大的元素。</param>
+        /// <param name="j">判断是否较小的元素。</param>
+        /// <returns></returns>
+        private bool Greater(int i, int j)
+        {
+            int cmp = this.pq[i].CompareTo(this.pq[j]);
+            if (cmp == 0)
+                return this.time[i].CompareTo(this.time[j]) > 0;
+            return cmp > 0;
+        }
+            
+
+        /// <summary>
+        /// 交换堆中的两个元素。
+        /// </summary>
+        /// <param name="i">要交换的第一个元素下标。</param>
+        /// <param name="j">要交换的第二个元素下标。</param>
+        protected virtual void Exch(int i, int j)
+        {
+            Key swap = this.pq[i];
+            this.pq[i] = this.pq[j];
+            this.pq[j] = swap;
+
+            long temp = this.time[i];
+            this.time[i] = this.time[j];
+            this.time[j] = temp;
+        }
+
+        /// <summary>
+        /// 检查当前二叉树是不是一个最小堆。
+        /// </summary>
+        /// <returns></returns>
+        private bool IsMinHeap() => IsMinHeap(1);
+
+        /// <summary>
+        /// 确定以 k 为根节点的二叉树是不是一个最小堆。
+        /// </summary>
+        /// <param name="k">需要检查的二叉树根节点。</param>
+        /// <returns></returns>
+        private bool IsMinHeap(int k)
+        {
+            if (k > this.n)
+                return true;
+            int left = 2 * k;
+            int right = 2 * k + 1;
+            if (left <= this.n && Greater(k, left))
+                return false;
+            if (right <= this.n && Greater(k, right))
+                return false;
+
+            return IsMinHeap(left) && IsMinHeap(right);
+        }
+    }
+}

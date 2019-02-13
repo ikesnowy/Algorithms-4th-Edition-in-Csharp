e13427f5b338efa2577604514fc3319f96c4ac39
沈星繁 <ikesnowy@outlook.com>
2019-02-13 17:15:19 +0800

    #444, 3.1.4 Finished

---
+++ 3 Searching/3.1/3.1.4/Event.cs
@@ -0,0 +1,19 @@
+﻿namespace _3._1._4
+{
+    public class Event
+    {
+        public string EventMessage { get; set; }
+
+        public Event() : this(null) { }
+
+        public Event(string message)
+        {
+            this.EventMessage = message;
+        }
+
+        public override string ToString()
+        {
+            return this.EventMessage;
+        }
+    }
+}

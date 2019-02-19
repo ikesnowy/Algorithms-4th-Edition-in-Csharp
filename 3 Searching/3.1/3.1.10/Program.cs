2fe071fb6edd97130d29d5fd6e7110c90ea3ad19
沈星繁 <ikesnowy@outlook.com>
2019-02-19 21:09:11 +0800

    #450, Fix Mistaken Compare Time

--- 3 Searching/3.1/3.1.10/Program.cs
+++ 3 Searching/3.1/3.1.10/Program.cs
@@ -13,20 +13,20 @@
         static void Main(string[] args)
         {
             // k v  list                                    compare
-            // E 0  E0                                      1
-            // A 1  A1 E0                                   2
-            // S 2  S2 A1 E0                                3
-            // Y 3  Y3 S2 A1 E0                             4
-            // Q 4  Q4 Y3 S2 A1 E0                          5
-            // U 5  U5 Q4 Y3 S2 A1 E0                       6
+            // E 0  E0                                      0
+            // A 1  A1 E0                                   1
+            // S 2  S2 A1 E0                                2
+            // Y 3  Y3 S2 A1 E0                             3
+            // Q 4  Q4 Y3 S2 A1 E0                          4
+            // U 5  U5 Q4 Y3 S2 A1 E0                       5
             // E 6  U5 Q4 Y3 S2 A1 E6                       6
             // S 7  U5 Q4 Y3 S7 A1 E6                       4
-            // T 8  T8 U5 Q4 Y3 S7 A1 E6                    7
-            // I 9  I9 T8 U5 Q4 Y3 S7 A1 E6                 8
-            // O 10 O10 I9 T8 U5 Q4 Y3 S7 A1 E6             9
-            // N 11 N11 O10 I9 T8 U5 Q4 Y3 S7 A1 E6         10
+            // T 8  T8 U5 Q4 Y3 S7 A1 E6                    6
+            // I 9  I9 T8 U5 Q4 Y3 S7 A1 E6                 7
+            // O 10 O10 I9 T8 U5 Q4 Y3 S7 A1 E6             8
+            // N 11 N11 O10 I9 T8 U5 Q4 Y3 S7 A1 E6         9
             // 
-            // 共比较 65 次。
+            // 共比较 55 次。
         }
     }
 }

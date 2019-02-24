a056c25e88981967d59de9fcd6ce58fddfd6ffd1
沈星繁 <ikesnowy@outlook.com>
2019-02-24 19:06:51 +0800

    #457, 3.1.17 Finished.

---
+++ 3 Searching/3.1/3.1.17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SymbolTable;
+
+namespace _3._1._17
+{
+    /*
+     * 3.1.17
+     * 
+     * 为 BinarySearchST 实现 floor() 方法。
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 官方实现：https://algs4.cs.princeton.edu/31elementary/BinarySearchST.java.html
+
+            BinarySearchST<string, string> st = new BinarySearchST<string, string>();
+            st.Put("alpha", "α");
+            st.Put("beta", "β");
+            st.Put("gamma", "γ");
+
+            Console.WriteLine(st.Floor("delta"));
+        }
+    }
+}

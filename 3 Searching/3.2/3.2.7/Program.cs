6ea2799310f05fb5bc02fc17b5a42913c71f936f
沈星繁 <ikesnowy@outlook.com>
2019-06-13 22:38:42 +0800

    Update dev-pipeline

--- 3 Searching/3.2/3.2.7/Program.cs
+++ 3 Searching/3.2/3.2.7/Program.cs
@@ -21,7 +21,7 @@ namespace _3._2._7
                 constantAvgCompare.Put(key, key);
             }
 
-            Console.WriteLine("Recursive AvgCompare Method:");
+            Console.WriteLine("Recursive AvgCompare Method");
             Console.WriteLine(recursiveAvgCompare);
             Console.WriteLine("Avg Compare: " + recursiveAvgCompare.AverageCompares());
 

872ed4012b16079d73218d471d4a4f9d14e40d72
沈星繁 <ikesnowy@outlook.com>
2019-06-19 14:24:11 +0800

    Migrate API Docs and Markdown Builder

--- MarkdownBuilder/Program.cs
+++ MarkdownBuilder/Program.cs
@@ -15,25 +15,25 @@ namespace MarkdownBuilder
 
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("# 写在前面")
-              .AppendLine()
-              .AppendLine("整个项目都托管在了 Github 上：<https://github.com/ikesnowy/Algorithms-4th-Edition-in-Csharp>")
-              .AppendLine("查找更方便的版本见：<https://alg4.ikesnowy.com/>")
-              .AppendLine("这一节内容可能会用到的库文件有 SymbolTable，同样在 Github 上可以找到。")
-              .AppendLine("善用 Ctrl + F 查找题目。")
-              .AppendLine()
-              .AppendLine("### 习题&题解")
-              .AppendLine();
+                .AppendLine()
+                .AppendLine("整个项目都托管在了 Github 上：<https://github.com/ikesnowy/Algorithms-4th-Edition-in-Csharp>")
+                .AppendLine("查找更方便的版本见：<https://alg4.ikesnowy.com/>")
+                .AppendLine("这一节内容可能会用到的库文件有 SymbolTable，同样在 Github 上可以找到。")
+                .AppendLine("善用 Ctrl + F 查找题目。")
+                .AppendLine()
+                .AppendLine("### 习题&题解")
+                .AppendLine();
 
             for (int i = 1; i <= questionNum; i++)
             {
                 sb.AppendLine(section + "." + i)
-                  .AppendLine()
-                  .AppendLine("##### 解答")
-                  .AppendLine()
-                  .AppendLine("##### 代码")
-                  .AppendLine()
-                  .AppendLine("##### 另请参阅")
-                  .AppendLine();
+                    .AppendLine()
+                    .AppendLine("##### 解答")
+                    .AppendLine()
+                    .AppendLine("##### 代码")
+                    .AppendLine()
+                    .AppendLine("##### 另请参阅")
+                    .AppendLine();
             }
 
             StreamWriter sw = new StreamWriter(File.OpenWrite(section + ".md"));

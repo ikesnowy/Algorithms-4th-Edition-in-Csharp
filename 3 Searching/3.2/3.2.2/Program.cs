1487a725f3a5bbb266bcfe6965fa114f4f8d3360
沈星繁 <ikesnowy@outlook.com>
2019-06-29 18:10:00 +0800

    3.2.11 Finished

--- 3 Searching/3.2/3.2.2/Program.cs
+++ 3 Searching/3.2/3.2.2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BinarySearchTree;
 
 namespace _3._2._2
 {
@@ -6,7 +7,26 @@ namespace _3._2._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] worstInput =
+            {
+                "A X C S E R H",
+                "X A S C R E H",
+                "A C E H R S X",
+                "X S R H E C A",
+                "X A S R H E C",
+                "A X S R H E C"
+            };
+
+            foreach (var worst in worstInput)
+            {
+                var keys = worst.Split(' ');
+                var bst = new BST<string, string>();
+                foreach (var key in keys)
+                {
+                    bst.Put(key, key);
+                }
+                Console.WriteLine(bst);
+            }
         }
     }
 }

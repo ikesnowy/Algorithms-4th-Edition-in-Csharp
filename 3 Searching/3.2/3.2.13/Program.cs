a0ccbf6bd68f72c3908e9058747a11752120fa06
沈星繁 <ikesnowy@outlook.com>
2019-07-27 16:44:15 +0800

    3.2.13 Finished

---
+++ 3 Searching/3.2/3.2.13/Program.cs
@@ -0,0 +1,13 @@
+﻿using BinarySearchTree;
+using System;
+
+namespace _3._2._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BSTTester.Test(new BSTNonRecursive<string, int>());
+        }
+    }
+}

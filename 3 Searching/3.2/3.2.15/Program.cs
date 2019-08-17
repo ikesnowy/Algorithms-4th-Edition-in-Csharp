87b8697c11e8eb074b2a992ce22bf38480d7b5d4
沈星繁 <ikesnowy@outlook.com>
2019-08-17 21:59:26 +0800

    3.2.15 Finished

---
+++ 3 Searching/3.2/3.2.15/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _3._2._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * floor(Q): E Q
+             * select(5): E Q
+             * ceiling(Q): E Q
+             * rank(J): E Q J
+             * size(D, T): E Q T E D
+             * keys(D, T): E D E Q J J M Q T S S T
+             *
+             */
+        }
+    }
+}

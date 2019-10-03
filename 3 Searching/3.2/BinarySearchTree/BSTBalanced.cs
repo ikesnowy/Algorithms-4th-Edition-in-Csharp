a82bb0d96a2f2845e2f3e75b5a3798e5e8acc6df
沈星繁 <ikesnowy@outlook.com>
2019-10-03 21:06:29 +0800

    3.2.26 Finished

--- 3 Searching/3.2/BinarySearchTree/BSTBalanced.cs
+++ 3 Searching/3.2/BinarySearchTree/BSTBalanced.cs
@@ -5,12 +5,23 @@ namespace BinarySearchTree
 {
     public class BSTBalanced<TKey, TValue> : BST<TKey, TValue> where TKey : IComparable<TKey>
     {
+        /// <summary>
+        /// 构造一棵平衡的二叉搜索树。
+        /// </summary>
+        /// <param name="init">初始键值对。</param>
         public BSTBalanced(KeyValuePair<TKey, TValue>[] init)
         {
             Array.Sort(init, (left, right) => left.Key.CompareTo(right.Key));
             root = BuildTree(init, 0, init.Length - 1);
         }
 
+        /// <summary>
+        /// 根据给定的中序序列递归地构造一棵平衡的二叉搜索树。
+        /// </summary>
+        /// <param name="init">有序键值对。</param>
+        /// <param name="lo">构造范围的下标最小值。</param>
+        /// <param name="hi">构造范围的下标最大值。</param>
+        /// <returns>构造完毕后二叉搜索树的根结点。</returns>
         private Node BuildTree(KeyValuePair<TKey, TValue>[] init, int lo, int hi)
         {
             if (lo > hi)

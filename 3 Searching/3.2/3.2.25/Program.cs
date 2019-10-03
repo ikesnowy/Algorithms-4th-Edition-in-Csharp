a88c6c12fbb5bd07c40b8412105a0e68cb0f3d9d
沈星繁 <ikesnowy@outlook.com>
2019-10-03 18:36:17 +0800

    3.2.25 Finished

---
+++ 3 Searching/3.2/3.2.25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using BinarySearchTree;
+
+namespace _3._2._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new[]
+            {
+                new KeyValuePair<int, int>(6, 6),
+                new KeyValuePair<int, int>(4, 4),
+                new KeyValuePair<int, int>(8, 8),
+                new KeyValuePair<int, int>(3, 3),
+                new KeyValuePair<int, int>(1, 1),
+                new KeyValuePair<int, int>(7, 7)
+            };
+            var bst = new BSTBalanced<int, int>(data);
+            Console.WriteLine(bst);
+        }
+    }
+}
---
+++ 3 Searching/3.2/BinarySearchTree/BSTBalanced.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchTree
+{
+    public class BSTBalanced<TKey, TValue> : BST<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        public BSTBalanced(KeyValuePair<TKey, TValue>[] init)
+        {
+            Array.Sort(init, (left, right) => left.Key.CompareTo(right.Key));
+            root = BuildTree(init, 0, init.Length - 1);
+        }
+
+        private Node BuildTree(KeyValuePair<TKey, TValue>[] init, int lo, int hi)
+        {
+            if (lo > hi)
+            {
+                return null;
+            }
+
+            var mid = (hi - lo) / 2 + lo;
+            var current = new Node(init[mid].Key, init[mid].Value, 1);
+            current.Left = BuildTree(init, lo, mid - 1);
+            current.Right = BuildTree(init, mid + 1, hi);
+            if (current.Left != null)
+            {
+                current.Size += current.Left.Size;
+            }
+
+            if (current.Right != null)
+            {
+                current.Size += current.Right.Size;
+            }
+            return current;
+        }
+    }
+}

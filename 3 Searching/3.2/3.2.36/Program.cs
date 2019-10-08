5c06d024b01ed344a4e87970881028f110cc934b
沈星繁 <ikesnowy@outlook.com>
2019-10-08 20:48:34 +0800

    3.2.36 Finished

---
+++ 3 Searching/3.2/3.2.36/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BinarySearchTree;
+
+namespace _3._2._36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bst = new BSTNonRecursive<int, int>();
+            bst.Put(10, 10);
+            bst.Put(4, 4);
+            bst.Put(12, 12);
+            bst.Put(9, 9);
+            bst.Put(3, 3);
+            bst.Put(8, 8);
+
+            foreach (var key in bst.Keys(5, 11))
+            {
+                Console.Write(key + ", ");
+            }
+        }
+    }
+}

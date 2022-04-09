4691b08fd6529f734e2471834625625ac930d033
沈星繁 <ikesnowy@outlook.com>
2022-04-09 19:42:01 +0800

    fix: remove clipboard selection code

--- docs/themes/hugo-book-modified/assets/clipboard.js
+++ docs/themes/hugo-book-modified/assets/clipboard.js
@@ -9,13 +9,14 @@
     selection.addRange(range);
   }
 
-  document.querySelectorAll("pre code").forEach(code => {
-    code.addEventListener("click", function (event) {
-      select(code.parentElement);
+  // #613 do nothing here
+  // document.querySelectorAll("pre code").forEach(code => {
+  //   code.addEventListener("click", function (event) {
+  //     select(code.parentElement);
 
-      if (navigator.clipboard) {
-        navigator.clipboard.writeText(code.parentElement.textContent);
-      }
-    });
-  });
+  //     if (navigator.clipboard) {
+  //       navigator.clipboard.writeText(code.parentElement.textContent);
+  //     }
+  //   });
+  // });
 })();

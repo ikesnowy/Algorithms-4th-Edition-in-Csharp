71c632dcb7317553dbb9765d4a2a297da726b123
沈星繁 <ikesnowy@outlook.com>
2022-06-25 18:44:08 +0800

    feat: 3-3-14 finished

--- 3 Searching/3.3/3.3.13/Program.cs
+++ 3 Searching/3.3/3.3.13/Program.cs
@@ -1,2 +1,3 @@
 ﻿// 官网解答见：https://algs4.cs.princeton.edu/33balanced/
+// 具体证明见：/docs/content/3-3-13.md
 Console.WriteLine("True");
---
+++ 3 Searching/3.3/3.3.14/Program.cs
@@ -0,0 +1,10 @@
+﻿using BalancedSearchTree;
+
+// 具体证明见上一题的解答。
+var bst = new RedBlackBst<string, int>();
+var input = "A B C D E F G H I J K".Split(' ');
+foreach (var s in input)
+{
+    bst.Put(s, 1);
+    Console.WriteLine(bst);
+}

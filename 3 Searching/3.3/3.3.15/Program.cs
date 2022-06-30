7ed7ff821bbb8ef47147135ef65995b69f1f277a
沈星繁 <ikesnowy@outlook.com>
2022-06-30 19:26:53 +0800

    feat: 3.3.15 finished

---
+++ 3 Searching/3.3/3.3.15/Program.cs
@@ -0,0 +1,10 @@
+﻿using BalancedSearchTree;
+
+// 具体证明见 3.3.13 的解答，倒序插入无法保证高度递增。
+var bst = new RedBlackBst<string, int>();
+var input = "A B C D E F G H I J K".Split(' ').Reverse();
+foreach (var s in input)
+{
+    bst.Put(s, 1);
+    Console.WriteLine(bst);
+}
